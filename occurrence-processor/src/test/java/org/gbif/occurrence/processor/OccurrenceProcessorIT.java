ddf101aeb870fd4dee177613fe95b788a244c897
Oliver Meyn <oliver@mineallmeyn.com>
2014-02-25 14:15:46 +0100

    updated occProcIT for gbifID and dwca json fragment

--- occurrence-processor/src/test/java/org/gbif/occurrence/processor/OccurrenceProcessorIT.java
+++ occurrence-processor/src/test/java/org/gbif/occurrence/processor/OccurrenceProcessorIT.java
@@ -1,5 +1,6 @@
 package org.gbif.occurrence.processor;
 
+import org.gbif.api.model.crawler.DwcaValidationReport;
 import org.gbif.api.model.occurrence.Occurrence;
 import org.gbif.api.vocabulary.BasisOfRecord;
 import org.gbif.api.vocabulary.Country;
@@ -65,12 +66,13 @@ public class OccurrenceProcessorIT {
   private static String abcd206Multi;
   private static String dwc14;
   private static String dwc14_modified;
+  private static String dwcaSingle;
 
   private static final String BGBM_KEY = "57254bd0-8256-11d8-b7ed-b8a03c50a862";
   private static final String BOGART_DATASET_KEY = "85697f04-f762-11e1-a439-00145eb45e9a";
+  private static final String PONTAURUS_DATASET_KEY = "8575f23e-f762-11e1-a439-00145eb45e9a";
 
-  private static final String MAINZ_MUSEUM_KEY = "33aecde5-7e13-4272-9cb7-f4f3b0eb820c";
-  private static final String MAINZ_ZOO_DATASET_KEY = "7e2989f0-f762-11e1-a439-00145eb45e9a";
+
 
   @BeforeClass
   public static void preClass() throws IOException {
@@ -78,6 +80,7 @@ public class OccurrenceProcessorIT {
     abcd206Multi = Resources.toString(Resources.getResource("abcd206_multi.xml"), Charsets.UTF_8);
     dwc14 = Resources.toString(Resources.getResource("dwc14.xml"), Charsets.UTF_8);
     dwc14_modified = Resources.toString(Resources.getResource("dwc14_modified.xml"), Charsets.UTF_8);
+    dwcaSingle = Resources.toString(Resources.getResource("fragment.json"), Charsets.UTF_8);
   }
 
   @Before
@@ -115,10 +118,50 @@ public class OccurrenceProcessorIT {
     curator.close();
   }
 
-  // TODO: test endtoend using new parse and interp msgs, both will be updates
+  @Test
+  public void testEndToEndDwca() throws IOException, InterruptedException {
+    UUID datasetKey = UUID.fromString(PONTAURUS_DATASET_KEY);
+    OccurrenceSchemaType xmlSchema = OccurrenceSchemaType.DWCA;
+    Integer crawlId = 1;
+    DwcaValidationReport report = new DwcaValidationReport(datasetKey, 1, 1, 0, 1, 0, true);
+    OccurrenceFragmentedMessage msg =
+      new OccurrenceFragmentedMessage(datasetKey, crawlId, dwcaSingle.getBytes(), xmlSchema, EndpointType.DWC_ARCHIVE,
+        report);
+    messagePublisher.send(msg);
+
+    TimeUnit.MILLISECONDS.sleep(5000);
+
+    Occurrence got = occurrenceService.get(1);
+    assertNotNull(got);
+    assertEquals("BGBM", got.getVerbatimField(DwcTerm.institutionCode));
+    assertEquals("Pontaurus", got.getVerbatimField(DwcTerm.collectionCode));
+    assertEquals("988", got.getVerbatimField(DwcTerm.catalogNumber));
+    assertEquals(datasetKey, got.getDatasetKey());
+    // note: this is set here inside the occ project, but from a ws call the serializer will omit these 'superseded' terms
+    assertEquals("Verbascum cheiranthifolium var. cheiranthifolium", got.getVerbatimField(DwcTerm.scientificName));
+    assertEquals("Verbascum cheiranthifolium Boiss.", got.getScientificName());
+    assertEquals(37.42123, got.getDecimalLatitude().doubleValue(), 0.000001);
+    assertEquals(34.56812, got.getDecimalLongitude().doubleValue(), 0.000001);
+    assertEquals(Country.fromIsoCode("TR"), got.getCountry());
+    Calendar c = Calendar.getInstance();
+    c.set(1999, 6, 30);
+    c.set(Calendar.HOUR_OF_DAY, 0);
+    c.set(Calendar.MINUTE, 0);
+    c.set(Calendar.SECOND, 0);
+    c.set(Calendar.MILLISECOND, 0);
+    assertEquals(c.getTimeInMillis(), got.getEventDate().getTime());
+    assertEquals(BasisOfRecord.PRESERVED_SPECIMEN, got.getBasisOfRecord());
+    assertEquals("Markus Döring", got.getVerbatimField(DwcTerm.identifiedBy));
+
+    assertEquals(BGBM_KEY, got.getPublishingOrgKey().toString());
+    assertEquals(Country.GERMANY, got.getPublishingCountry());
+    assertEquals(EndpointType.DWC_ARCHIVE, got.getProtocol());
+    assertEquals("1", got.getVerbatimField(GbifTerm.gbifID));
+    assertEquals("ABC123", got.getVerbatimField(DwcTerm.occurrenceID));
+  }
 
   @Test
-  public void testEndToEnd() throws IOException, InterruptedException {
+  public void testEndToEndAbcd2() throws IOException, InterruptedException {
     UUID datasetKey = UUID.fromString(BOGART_DATASET_KEY);
     OccurrenceSchemaType xmlSchema = OccurrenceSchemaType.ABCD_2_0_6;
     Integer crawlId = 1;
@@ -127,7 +170,7 @@ public class OccurrenceProcessorIT {
         null);
     messagePublisher.send(msg);
 
-    TimeUnit.MILLISECONDS.sleep(15000);
+    TimeUnit.MILLISECONDS.sleep(5000);
 
     Occurrence got = occurrenceService.get(1);
     assertNotNull(got);
@@ -135,7 +178,6 @@ public class OccurrenceProcessorIT {
     assertEquals("AlgaTerra", got.getVerbatimField(DwcTerm.collectionCode));
     assertEquals("5834", got.getVerbatimField(DwcTerm.catalogNumber));
     assertEquals(datasetKey, got.getDatasetKey());
-
     assertEquals("Tetraedron caudatum (Corda) Hansgirg", got.getScientificName());
     assertEquals(52.423798, got.getDecimalLatitude().doubleValue(), 0.00001);
     assertEquals(13.191434, got.getDecimalLongitude().doubleValue(), 0.00001);
