0e74798dc39bab14360e59dd1f95cd9639d550aa
Federico Mendez <federicomh@gmail.com>
2014-03-07 12:12:52 +0100

    removing query.q, replaced by create_occurrence_table.q install.sh: exit on any failure

--- occurrence-download-workflow/install.sh
+++ occurrence-download-workflow/install.sh
@@ -1,7 +1,10 @@
+#exit on any failure
+set -e
+
 #!/bin/bash
 ENV=$1
 P=$2
 
 mvn -P$P clean package assembly:single -Doccurrence.download.ws.password=$3
-hadoop dfs -rm -r -skipTrash /occurrence-download/$ENV
-hadoop dfs -put target/oozie-workflow /occurrence-download/$ENV
+hadoop fs -rm -r -skipTrash /occurrence-download/$ENV
+hadoop fs -put target/oozie-workflow /occurrence-download/$ENV
