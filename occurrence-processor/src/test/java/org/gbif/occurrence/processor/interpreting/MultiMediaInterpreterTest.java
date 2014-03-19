c9d778ccf80c15dc48311fd27552251ce7655a27
Markus Döring <mdoering@gbif.org>
2014-03-19 17:52:10 +0100

    Adding full Media interpreter tests

--- occurrence-processor/src/test/java/org/gbif/occurrence/processor/interpreting/MultiMediaInterpreterTest.java
+++ occurrence-processor/src/test/java/org/gbif/occurrence/processor/interpreting/MultiMediaInterpreterTest.java
@@ -1,19 +1,30 @@
 package org.gbif.occurrence.processor.interpreting;
 
 import org.gbif.api.model.common.MediaObject;
+import org.gbif.api.model.occurrence.Occurrence;
+import org.gbif.api.model.occurrence.VerbatimOccurrence;
+import org.gbif.api.model.occurrence.VerbatimRecord;
+import org.gbif.api.vocabulary.Extension;
 import org.gbif.api.vocabulary.MediaType;
+import org.gbif.dwc.terms.DcTerm;
+import org.gbif.dwc.terms.DwcTerm;
 
 import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.List;
 
+import com.beust.jcommander.internal.Lists;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 
 /**
  *
  */
 public class MultiMediaInterpreterTest {
+  private SimpleDateFormat ISO = new SimpleDateFormat("yyyy-MM-dd");
 
   @Test
   public void testDetectType() throws Exception {
@@ -40,6 +51,65 @@ public class MultiMediaInterpreterTest {
 
     return mo;
   }
+
+  @Test
+  public void testInterpretMediaCore() throws Exception {
+    VerbatimOccurrence v = new VerbatimOccurrence();
+    Occurrence o = new Occurrence();
+
+    v.setVerbatimField(DwcTerm.associatedMedia, "http://farm8.staticflickr.com/7093/7039524065_3ed0382368.jpg, http://www.flickr.com/photos/70939559@N02/7039524065");
+    MultiMediaInterpreter.interpretMedia(v,o);
+
+    assertEquals(2, o.getMedia().size());
+    for (MediaObject m : o.getMedia()) {
+      assertEquals(MediaType.StillImage, m.getType());
+      assertEquals("image/jpeg", m.getFormat());
+      assertNotNull(m.getUrl());
+    }
+  }
+
+  @Test
+  public void testInterpretMediaExtension() throws Exception {
+    VerbatimOccurrence v = new VerbatimOccurrence();
+    Occurrence o = new Occurrence();
+
+    List<VerbatimRecord> media = Lists.newArrayList();
+    v.getExtensions().put(Extension.IMAGE, media);
+
+    VerbatimRecord rec = new VerbatimRecord();
+    rec.setField(DcTerm.identifier, "http://farm8.staticflickr.com/7093/7039524065_3ed0382368.jpg");
+    rec.setField(DcTerm.references, "http://www.flickr.com/photos/70939559@N02/7039524065");
+    rec.setField(DcTerm.format, "jpg");
+    rec.setField(DcTerm.title, "Geranium Plume Moth 0032");
+    rec.setField(DcTerm.description, "Geranium Plume Moth 0032 description");
+    rec.setField(DcTerm.license, "BY-NC-SA 2.0");
+    rec.setField(DcTerm.creator, "Moayed Bahajjaj");
+    rec.setField(DcTerm.created, "2012-03-29");
+    media.add(rec);
+
+    rec = new VerbatimRecord();
+    rec.setField(DcTerm.identifier, "http://www.flickr.com/photos/70939559@N02/7039524065.jpg");
+    media.add(rec);
+
+    MultiMediaInterpreter.interpretMedia(v,o);
+
+    assertEquals(2, o.getMedia().size());
+    for (MediaObject m : o.getMedia()) {
+      assertEquals(MediaType.StillImage, m.getType());
+      assertNotNull(m.getUrl());
+    }
+
+    assertEquals(MediaType.StillImage, o.getMedia().get(0).getType());
+    assertEquals("image/jpeg", o.getMedia().get(0).getFormat());
+    assertEquals("Geranium Plume Moth 0032", o.getMedia().get(0).getTitle());
+    assertEquals("Geranium Plume Moth 0032 description", o.getMedia().get(0).getDescription());
+    assertEquals("BY-NC-SA 2.0", o.getMedia().get(0).getLicense());
+    assertEquals("Moayed Bahajjaj", o.getMedia().get(0).getCreator());
+    assertEquals("2012-03-29", ISO.format(o.getMedia().get(0).getCreated()));
+    assertEquals("http://www.flickr.com/photos/70939559@N02/7039524065", o.getMedia().get(0).getReferences().toString());
+    assertEquals("http://farm8.staticflickr.com/7093/7039524065_3ed0382368.jpg", o.getMedia().get(0).getUrl().toString());
+  }
+
   @Test
   public void testParseAssociatedMedia() throws Exception {
     assertEquals(0, MultiMediaInterpreter.parseAssociatedMedia(null).size());
