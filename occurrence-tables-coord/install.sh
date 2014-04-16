9d3cfed5710d4c932081db25e657ad815b43dfe6
Federico Mendez <federicomh@gmail.com>
2014-04-16 13:56:30 +0200

    the script removes the current coordinator job if there’s one running

--- occurrence-tables-coord/install.sh
+++ occurrence-tables-coord/install.sh
@@ -6,6 +6,13 @@ ENV=$1
 P=$2
 OOZIE=$3
 
+#gets the oozie id of the current coordinator job if it exists
+WID=$(oozie jobs -oozie $OOZIE -jobtype coordinator -filter name=OccurrenceHDFSBuild-$ENV\;status=RUNNING |  awk 'NR==3' | awk '{print $1;}')
+if [ -n "$WID" ]; then
+  echo "Killing current coordinator job" $WID
+  oozie job -oozie $OOZIE -kill $WID
+fi
+
 mvn -P$P clean package assembly:single
 hadoop fs -rm -r -skipTrash /occurrence-tables-coord/$ENV
 hadoop fs -put target/oozie-workflow /occurrence-tables-coord/$ENV
