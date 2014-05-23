33894293b1028e9d0a997a8e9148dfb750362fd9
Federico Mendez <federicomh@gmail.com>
2014-05-23 16:52:52 +0200

    profile added to customise hbase-indexer scripts morphlines configuration files use a maven variable for the hbase table name log4j.properties added to avoid use the cloudera example file used previously

--- occurrence-index-builder-workflow/bin/hbase_indexing/runHBaseBatchIndexer.sh
+++ occurrence-index-builder-workflow/bin/hbase_indexing/runHBaseBatchIndexer.sh
@@ -2,5 +2,5 @@
 ZK_HOST=$1
 #occurrence
 SOLR_COLLECTION=$2
-export HADOOP_CLASSPATH=${maven.dependency.org.gbif.gbif-api.jar}:occurrence-common-0.18-SNAPSHOT.jar:occurrence-search-0.18-SNAPSHOT.jar:/opt/cloudera/parcels/SOLR/lib/hbase-solr/lib/*
-hadoop --config /etc/hadoop/conf jar /opt/cloudera/parcels/SOLR/lib/hbase-solr/tools/hbase-indexer-mr-*-job.jar --conf /etc/hbase/conf/hbase-site.xml -D 'mapred.child.java.opts=-Xmx500m' -libjars /opt/cloudera/parcels/SOLR/lib/solr/server/webapps/solr/WEB-INF/lib/jts-1.13.jar,gbif-api-0.14-SNAPSHOT.jar,occurrence-common-0.18-SNAPSHOT.jar,occurrence-search-0.18-SNAPSHOT.jar --hbase-indexer-file hbase_occurrence_batch_morphline.xml --zk-host $ZK_HOST --collection $SOLR_COLLECTION --go-live --log4j /opt/cloudera/parcels/SOLR/share/doc/search-1.2.0/examples/solr-nrt/log4j.properties
+export HADOOP_CLASSPATH=gbif-api-${gbif-api.version}.jar:occurrence-common-${parent.version}.jar:occurrence-search-${parent.version}.jar:/opt/cloudera/parcels/SOLR/lib/hbase-solr/lib/*
+hadoop --config /etc/hadoop/conf jar /opt/cloudera/parcels/SOLR/lib/hbase-solr/tools/hbase-indexer-mr-*-job.jar --conf /etc/hbase/conf/hbase-site.xml -D 'mapred.child.java.opts=-Xmx500m' -libjars /opt/cloudera/parcels/SOLR/lib/solr/server/webapps/solr/WEB-INF/lib/jts-1.13.jar,gbif-api-${gbif-api.version}.jar,occurrence-common-${parent.version}.jar,occurrence-search-${parent.version}.jar --hbase-indexer-file hbase_occurrence_batch_morphline.xml --zk-host $ZK_HOST --collection $SOLR_COLLECTION --go-live --log4j log4j.properties
