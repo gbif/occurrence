febf3dd67c6db3b705a4caeae175cbe87dc7b226
Oliver Meyn <oliver@mineallmeyn.com>
2014-06-11 14:45:54 +0200

    removing all references to hbase-indexer-engine, which blocks release

--- occurrence-search/src/main/java/org/gbif/occurrence/hbaseindexer/IntKeyFormatter.java
+++ occurrence-search/src/main/java/org/gbif/occurrence/hbaseindexer/IntKeyFormatter.java
@@ -1,25 +1,18 @@
 package org.gbif.occurrence.hbaseindexer;
 
-import com.google.common.primitives.Ints;
-import com.ngdata.hbaseindexer.uniquekey.BaseUniqueKeyFormatter;
-import com.ngdata.hbaseindexer.uniquekey.UniqueKeyFormatter;
-import org.apache.commons.codec.DecoderException;
-import org.apache.commons.codec.binary.Hex;
-
-
-public class IntKeyFormatter extends BaseUniqueKeyFormatter implements UniqueKeyFormatter {
-
-  @Override
-  protected String encodeAsString(byte[] bytes) {
-    return new Integer(Ints.fromByteArray(bytes)).toString();
-  }
-
-  @Override
-  protected byte[] decodeFromString(String value) {
-    try {
-      return Ints.toByteArray(Ints.fromByteArray(Hex.decodeHex(value.toCharArray())));
-    } catch (DecoderException e) {
-      throw new IllegalArgumentException("Value '" + value + "' can't be decoded as hex", e);
-    }
-  }
-}
+//public class IntKeyFormatter extends BaseUniqueKeyFormatter implements UniqueKeyFormatter {
+//
+//  @Override
+//  protected String encodeAsString(byte[] bytes) {
+//    return new Integer(Ints.fromByteArray(bytes)).toString();
+//  }
+//
+//  @Override
+//  protected byte[] decodeFromString(String value) {
+//    try {
+//      return Ints.toByteArray(Ints.fromByteArray(Hex.decodeHex(value.toCharArray())));
+//    } catch (DecoderException e) {
+//      throw new IllegalArgumentException("Value '" + value + "' can't be decoded as hex", e);
+//    }
+//  }
+//}
