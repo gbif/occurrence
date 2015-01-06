471458fc138d423153423571ab1adb49e09542c7
Markus Döring <m.doering@mac.com>
2015-01-06 15:53:11 +0100

    clean install,sh

--- occurrence-download-workflow/install.sh
+++ occurrence-download-workflow/install.sh
@@ -8,10 +8,10 @@ TOKEN=$3
 VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep ^0.`
 
 echo "Get latest workflow config profiles from github"
-curl -H "Authorization: token $TOKEN" -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/gbif/gbif-configuration/contents/occurrence-download-workflow/$VERSION/profiles.xml
+curl -s -H "Authorization: token $TOKEN" -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/gbif/gbif-configuration/contents/occurrence-download-workflow/$VERSION/profiles.xml
 
 echo "Assembling workflow jar for $ENV"
-mvn --settings profiles.xml -P$P -DskipTests clean package assembly:single
+mvn --settings profiles.xml -P$P -DskipTests clean install package assembly:single
 
 echo "Copy workflow to hadoop"
 hadoop fs -rm -r -skipTrash /occurrence-download/$ENV
