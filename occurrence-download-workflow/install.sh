a0b991faf07a6b69f344cb06f5bffdac875afecd
Markus Döring <m.doering@mac.com>
2015-01-06 14:45:29 +0100

    Fix bash token var substitution

--- occurrence-download-workflow/install.sh
+++ occurrence-download-workflow/install.sh
@@ -8,7 +8,7 @@ TOKEN=$3
 VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep ^0.`
 
 echo "Get latest workflow config profiles from github"
-curl -H 'Authorization: token $TOKEN' -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/gbif/gbif-configuration/contents/occurrence-download-workflow/$VERSION/profiles.xml
+curl -H "Authorization: token $TOKEN" -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/gbif/gbif-configuration/contents/occurrence-download-workflow/$VERSION/profiles.xml
 
 echo "Assembling workflow jar for $ENV"
 mvn --settings profiles.xml -P$P -DskipTests clean package assembly:single
