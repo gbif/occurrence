053ab149b97217a35cba9a69c1fcf6c9d988d429
Markus Döring <m.doering@mac.com>
2015-01-06 14:36:34 +0100

    Use simpler OAuth token from github to retrieve configs

--- occurrence-download-workflow/install.sh
+++ occurrence-download-workflow/install.sh
@@ -4,23 +4,14 @@ set -e
 #!/bin/bash
 ENV=$1
 P=$2
-GIT_CREDENTIALS=$3
+TOKEN=$3
 VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep ^0.`
 
-# we use settings from a provate github repo with secret passwords
-if [ -d "gbif-configuration" ]; then
-  #If gitrepo exists, update the repositories
-  echo "Updating local gbif-configuration Git repository"
-  cd gbif-configuration
-  git pull --all
-  cd ..
-else
-  echo "Cloning Git repository gbif-configuration"
-  git clone https://$GIT_CREDENTIALS@github.com/gbif/gbif-configuration
-fi
+echo "Get latest workflow config profiles from github"
+curl -H 'Authorization: token $TOKEN' -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/gbif/gbif-configuration/contents/occurrence-download-workflow/$VERSION/profiles.xml
 
 echo "Assembling workflow jar for $ENV"
-mvn --settings gbif-configuration/occurrence-download-workflow/$VERSION/profiles.xml -P$P -DskipTests clean package assembly:single
+mvn --settings profiles.xml -P$P -DskipTests clean package assembly:single
 
 echo "Copy workflow to hadoop"
 hadoop fs -rm -r -skipTrash /occurrence-download/$ENV
