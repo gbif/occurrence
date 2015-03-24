328bc19a7407f052c45e9472b06546ad88cb6252
Oliver Meyn <oliver@mineallmeyn.com>
2015-03-24 14:20:26 +0100

    change hadoop fs to hdfs dfs because hadoop style is deprecated

--- occurrence-index-builder-workflow/bin/hdfs_indexing/runSingleShardIndexer.sh
+++ occurrence-index-builder-workflow/bin/hdfs_indexing/runSingleShardIndexer.sh
@@ -7,9 +7,9 @@ cat jobsingleshard.properties | sed 's/\./_/g' > .properties.file
 . .properties.file
 rm .properties.file
 echo "Cleaning HDFS directory $oozieWfDestination"
-hadoop fs -rm -r -skipTrash $oozieWfDestination*
-echo "Copying workflow to HDFS directory $oozieWfDestination" 
-hadoop dfs -put ../../target/oozie-workflow/* $oozieWfDestination
+hdfs dfs -rm -r -skipTrash $oozieWfDestination*
+echo "Copying workflow to HDFS directory $oozieWfDestination"
+hdfs dfs -put ../../target/oozie-workflow/* $oozieWfDestination
 oozie_server=${oozie_server//[_]/.}
 echo "Running oozie workflow on server $oozie_server"
 oozie job -oozie $oozie_server -config jobsingleshard.properties -run
