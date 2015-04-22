6b1e7be571b4b58ee6523fb67c8c73ea73a190e0
Markus Döring <m.doering@mac.com>
2015-04-22 11:31:35 +0200

    Adding an interpret occ record command

---
+++ occurrence-cli/src/main/java/org/gbif/occurrence/cli/process/InterpretOccurrenceConfiguration.java
@@ -0,0 +1,23 @@
+package org.gbif.occurrence.cli.process;
+
+import org.gbif.common.messaging.config.MessagingConfiguration;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParametersDelegate;
+
+public class InterpretOccurrenceConfiguration {
+
+  @ParametersDelegate
+  @Valid
+  @NotNull
+  public MessagingConfiguration messaging = new MessagingConfiguration();
+
+  @Parameter(names = "--occurrence-key")
+  public Integer key;
+
+  @Parameter(names = "--occurrence-key-file")
+  public String keyFileName;
+}
