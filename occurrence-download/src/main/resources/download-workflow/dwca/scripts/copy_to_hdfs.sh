ab897db1da7f27f5be14cfe0b217462e988870ed
Federico Mendez <fmendez@gbif.org>
2015-05-08 00:21:26 +0200

    copy_to_hdfs.sh: workflow id is not used workflow.xml: adding execution permissions to .sh files

--- occurrence-download/src/main/resources/download-workflow/dwca/scripts/copy_to_hdfs.sh
+++ occurrence-download/src/main/resources/download-workflow/dwca/scripts/copy_to_hdfs.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-workflow_id=$1
+workflow_key=$1
 source_dir=$2
 hdfs_destination_dir=$3
 
-oozie_suffix='-oozie-oozi-W'
-file_name=${workflow_id//$oozie_suffix/}.zip
+file_name=${workflow_key}.zip
 
 hdfs dfs -put $source_dir/$file_name $hdfs_destination_dir/$file_name
