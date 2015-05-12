94dd661e9d6333398c23bff6000ab85eecd43bd7
Federico Mendez <fmendez@gbif.org>
2015-05-13 01:49:26 +0200

    missing download_table_name parameter added

--- occurrence-download/src/main/java/org/gbif/occurrence/download/file/dwca/oozie/ArchiveDownloadAction.java
+++ occurrence-download/src/main/java/org/gbif/occurrence/download/file/dwca/oozie/ArchiveDownloadAction.java
@@ -25,11 +25,13 @@ public class ArchiveDownloadAction {
     final String username = args[1];          // download user
     final String query = args[2];         // download query filter
     final boolean isSmallDownload = Boolean.parseBoolean(args[3]);    // isSmallDownload
+    final String downloadTableName = args[3];    // download table/file name
 
     WorkflowConfiguration workflowConfiguration = new WorkflowConfiguration();
 
     OccurrenceDownloadConfiguration configuration = new OccurrenceDownloadConfiguration.Builder()
       .withDownloadKey(downloadKey)
+      .withDownloadTableName(downloadTableName)
       .withFilter(query)
       .withIsSmallDownload(isSmallDownload)
       .withUser(username)
