31abf3ddbed0cb1766ae1b2fb2a4ca305689903d
Federico Mendez <fmendez@gbif.org>
2015-05-12 16:03:07 +0200

    removing test path

--- occurrence-download/src/main/java/org/gbif/occurrence/download/inject/DownloadWorkflowModule.java
+++ occurrence-download/src/main/java/org/gbif/occurrence/download/inject/DownloadWorkflowModule.java
@@ -41,7 +41,7 @@ import org.apache.hadoop.hbase.client.HTablePool;
  */
 public final class DownloadWorkflowModule extends AbstractModule {
 
-  public static final String CONF_FILE = "/Users/fmendez/dev/git/gbif/occurrence/occurrence-download/occurrence-download.properties";
+  public static final String CONF_FILE = "occurrence-download.properties";
 
   //Prefix for static settings
   public static final String PROPERTIES_PREFIX = "occurrence.download.";
