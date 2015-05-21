109d31bcc32e97c4e8235e543791a2448e0ffb78
Federico Mendez <fmendez@gbif.org>
2015-05-21 16:48:16 +0200

    javadoc

--- occurrence-download/src/main/java/org/gbif/occurrence/download/conf/WorkflowConfiguration.java
+++ occurrence-download/src/main/java/org/gbif/occurrence/download/conf/WorkflowConfiguration.java
@@ -13,17 +13,30 @@ import com.google.common.base.Throwables;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.CommonConfigurationKeys;
 
+/**
+ * Occurrence download workflow configuration.
+ * Contains the configuration settings that are accessible for all the executions of the download workflow.
+ * The configuration setting are read from the file DownloadWorkflowModule.CONF_FILE or are passed as constructor
+ * parameter as java Properties class.
+ */
 public class WorkflowConfiguration {
 
   private final Properties settings;
   private final Configuration hadoopConf;
 
+  /**
+   *
+   * @param settings properties class load with the workflow settings
+   */
   public WorkflowConfiguration(Properties settings) {
     this.settings = new Properties(settings);
     hadoopConf = new Configuration();
     hadoopConf.set(CommonConfigurationKeys.FS_DEFAULT_NAME_KEY, getHdfsNameNode());
   }
 
+  /**
+   * Loads the configuration from the file DownloadWorkflowModule.CONF_FILE.
+   */
   public WorkflowConfiguration() {
     try {
       settings = PropertiesUtil.loadProperties(DownloadWorkflowModule.CONF_FILE);
@@ -34,26 +47,47 @@ public class WorkflowConfiguration {
     }
   }
 
+  /**
+   *
+   * @return hdfs name node
+   */
   public String getHdfsNameNode() {
     Preconditions.checkNotNull(settings);
     return settings.getProperty(DownloadWorkflowModule.DefaultSettings.NAME_NODE_KEY);
   }
 
+  /**
+   *
+   * @return hive database name
+   */
   public String getHiveDb() {
     Preconditions.checkNotNull(settings);
     return settings.getProperty(DownloadWorkflowModule.DefaultSettings.HIVE_DB_KEY);
   }
 
+  /**
+   *
+   * @return  registry API url
+   */
   public String getRegistryWsUrl() {
     Preconditions.checkNotNull(settings);
     return settings.getProperty(DownloadWorkflowModule.DefaultSettings.REGISTRY_URL_KEY);
   }
 
+  /**
+   *
+   * @return local temp dir where downloads files are created
+   */
   public String getTempDir() {
     Preconditions.checkNotNull(settings);
     return settings.getProperty(DownloadWorkflowModule.DefaultSettings.TMP_DIR_KEY);
   }
 
+  /**
+   *
+   * @param downloadKey download id
+   * @return a link to the download file
+   */
   public String getDownloadLink(String downloadKey) {
     Preconditions.checkNotNull(settings);
     Preconditions.checkNotNull(downloadKey);
@@ -62,16 +96,28 @@ public class WorkflowConfiguration {
       .replace(DownloadUtils.DOWNLOAD_ID_PLACEHOLDER, downloadKey);
   }
 
+  /**
+   *
+   * @return hdfs directory where hive stores tables
+   */
   public String getHdfsOutputPath() {
     Preconditions.checkNotNull(settings);
     return settings.getProperty(DownloadWorkflowModule.DefaultSettings.HDFS_OUTPUT_PATH_KEY);
   }
 
+  /**
+   *
+   * @return hdfs directory of the Hive database
+   */
   public String getHiveDBPath() {
     Preconditions.checkNotNull(settings);
     return settings.getProperty(DownloadWorkflowModule.DefaultSettings.HIVE_DB_PATH_KEY);
   }
 
+  /**
+   *
+   * @return requested download format this value change for each execution
+   */
   public DownloadFormat getDownloadFormat() {
     Preconditions.checkNotNull(settings);
     String downloadFormat = settings.getProperty(DownloadWorkflowModule.DynamicSettings.DOWNLOAD_FORMAT_KEY);
@@ -81,15 +127,27 @@ public class WorkflowConfiguration {
     return null;
   }
 
+  /**
+   *
+   * @return GBIF API url
+   */
   public String getApiUrl() {
     Preconditions.checkNotNull(settings);
     return settings.getProperty(DownloadWorkflowModule.DefaultSettings.API_URL_KEY);
   }
 
+  /**
+   *
+   * @return HDFS Hadoop configuration
+   */
   public Configuration getHadoopConf() {
     return hadoopConf;
   }
 
+  /**
+   *
+   * @return properties class that contains the raw configuration settings
+   */
   public Properties getDownloadSettings() {
     return settings;
   }
