267bd40e333e65892f3aade5123e52e3e610120c
Federico Mendez <fmendez@gbif.org>
2015-11-05 12:20:01 +0100

    settings files are downloaded from the gbif-configuration repository

--- occurrence-index-builder-workflow/install-workflow.sh
+++ occurrence-index-builder-workflow/install-workflow.sh
@@ -7,14 +7,16 @@ TOKEN=$2
 
 echo "Getting latest occurrence-index-builder workflow properties file from github"
 curl -s -H "Authorization: token $TOKEN" -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/gbif/gbif-configuration/contents/occurrence-index-builder/$P.properties
+echo "Get latest tables-coord config profiles from github"
+curl -s -H "Authorization: token $TOKEN" -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/gbif/gbif-configuration/contents/occurrence-download/profiles.xml
 
 #extract the oozie.url value from the properties file
 oozie_url=`cat $P.properties| grep "oozie.url" | cut -d'=' -f2`
 
 echo "Assembling jar for $ENV"
 
-mvn -Poozie,$P clean package assembly:single
-mvn -Psolr,$P package assembly:single
+mvn --settings profiles.xml -Poozie,$P clean package assembly:single
+mvn --settings profiles.xml -Psolr,$P package assembly:single
 
 if hdfs dfs -test -d /occurrence-index-builder-$P/; then
    echo "Removing content of current Oozie workflow directory"
