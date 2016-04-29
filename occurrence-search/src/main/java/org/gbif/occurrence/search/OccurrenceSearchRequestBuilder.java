502531faea5ea15e5e6e365243549e9c3825a6ed
Federico Mendez <fmendez@gbif.org>
2016-04-29 17:10:07 +0200

    exlusion tag was misplaced, must be before the solr field declaration

--- occurrence-search/src/main/java/org/gbif/occurrence/search/OccurrenceSearchRequestBuilder.java
+++ occurrence-search/src/main/java/org/gbif/occurrence/search/OccurrenceSearchRequestBuilder.java
@@ -202,7 +202,7 @@ public class OccurrenceSearchRequestBuilder {
         return String
           .format(RANGE_FORMAT, bbox.getMinY() + "," + bbox.getMinX(), bbox.getMaxY() + "," + bbox.getMaxX());
       }
-      return SolrQueryUtils.taggedField(OccurrenceSolrField.COORDINATE.getFieldName()) + String.format(GEO_INTERSECTS_QUERY_FMT, geometry.toText());
+      return String.format(GEO_INTERSECTS_QUERY_FMT, geometry.toText());
     } catch (ParseException e) {
       throw new IllegalArgumentException(e);
     }
