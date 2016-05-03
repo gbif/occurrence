ee0a3a4ce6ca06d34f1494004eddec790bbe096a
Federico Mendez <fmendez@gbif.org>
2016-05-03 14:28:33 +0200

    adding missing mappings between search parameters and solr fields

--- occurrence-search/src/main/java/org/gbif/occurrence/search/solr/OccurrenceSolrField.java
+++ occurrence-search/src/main/java/org/gbif/occurrence/search/solr/OccurrenceSolrField.java
@@ -9,6 +9,8 @@ public enum OccurrenceSolrField {
   COUNTRY("country"), PUBLISHING_COUNTRY("publishing_country"), CONTINENT("continent"), YEAR("year"), MONTH("month"),
   CATALOG_NUMBER("catalog_number"), RECORDED_BY("recorded_by"), RECORD_NUMBER("record_number"),
   BASIS_OF_RECORD("basis_of_record"), DATASET_KEY("dataset_key"), TAXON_KEY("taxon_key"),
+  KINGDOM_KEY("kingdom_key"), PHYLUM_KEY("phylum_key"), CLASS_KEY("class_key"), ORDER_KEY("order_key"),
+  FAMILY_KEY("family_key"), GENUS_KEY("genus_key"), SUBGENUS_KEY("subgenus_key"), SPECIES_KEY("species_key"),
   COLLECTION_CODE("collection_code"), ELEVATION("elevation"), DEPTH("depth"), INSTITUTION_CODE("institution_code"),
   SPATIAL_ISSUES("spatial_issues"), HAS_COORDINATE("has_coordinate"), EVENT_DATE("event_date"),
   LAST_INTERPRETED("last_interpreted"), TYPE_STATUS("type_status"), MEDIA_TYPE("media_type"), ISSUE("issue"),
