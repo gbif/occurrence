b5c466a57f2fbbadfdb73675ebcf60836488fe5e
Federico Mendez <fmendez@gbif.org>
2016-05-20 13:53:52 +0200

    UDF removeNulls extended to remove empty strings

--- occurrence-hive/src/main/java/org/gbif/occurrence/hive/udf/ArrayNullsRemoverGenericUDF.java
+++ occurrence-hive/src/main/java/org/gbif/occurrence/hive/udf/ArrayNullsRemoverGenericUDF.java
@@ -10,6 +10,7 @@ import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector.Category;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspectorUtils;
+import org.apache.hadoop.hive.serde2.objectinspector.PrimitiveObjectInspector;
 import org.apache.hadoop.hive.serde2.objectinspector.StandardListObjectInspector;
 
 /**
@@ -18,33 +19,55 @@ import org.apache.hadoop.hive.serde2.objectinspector.StandardListObjectInspector
 public class ArrayNullsRemoverGenericUDF extends GenericUDF {
 
   private StandardListObjectInspector retValInspector;
+  private PrimitiveObjectInspector primitiveObjectInspector;
 
   @Override
-  public Object evaluate(DeferredObject[] arg0) throws HiveException {
-    List<?> list = retValInspector.getList(arg0[0].get());
-    List<?> result = Lists.newArrayList();
+  public Object evaluate(DeferredObject[] arguments) throws HiveException {
+    List list = retValInspector.getList(arguments[0].get());
+    List result = Lists.newArrayList();
     if (list != null && !list.isEmpty()) {
-      result = Lists.newArrayList(list);
-      result.removeAll(Collections.singleton(null));
+      if (primitiveObjectInspector != null && primitiveObjectInspector.getPrimitiveCategory() == PrimitiveObjectInspector.PrimitiveCategory.STRING) {
+        result = handlerStringArray(list);
+      } else {
+        result = Lists.newArrayList(list);
+        result.removeAll(Collections.singleton(null));
+      }
     }
     return result.isEmpty() ? null : result;
   }
 
-  @Override
-  public String getDisplayString(String[] arg0) {
-    return "removeNulls( " + arg0[0] + ")";
+  /**
+   * Removes empty Strings and null values.
+   */
+  private List handlerStringArray(List list) {
+    List result = Lists.newArrayList();
+    for (Object oElement : list) {
+      Object stdObject = ObjectInspectorUtils.copyToStandardJavaObject(oElement, primitiveObjectInspector);
+      if (stdObject != null && !((String)stdObject).trim().isEmpty()) {
+        result.add(stdObject);
+      }
+    }
+    return result;
   }
 
   @Override
-  public ObjectInspector initialize(ObjectInspector[] arg0)
-    throws UDFArgumentException {
-    if (arg0.length != 1) {
+  public String getDisplayString(String[] children) {
+    return "removeNulls( " + children[0] + ")";
+  }
+
+  @Override
+  public ObjectInspector initialize(ObjectInspector[] arguments) throws UDFArgumentException {
+    if (arguments.length != 1) {
       throw new UDFArgumentException("removeNulls takes an array as argument");
     }
-    if (arg0[0].getCategory() != Category.LIST) {
+    if (arguments[0].getCategory() != Category.LIST) {
       throw new UDFArgumentException("removeNulls takes an array as argument");
     }
-    retValInspector = (StandardListObjectInspector) ObjectInspectorUtils.getStandardObjectInspector(arg0[0]);
+    retValInspector = (StandardListObjectInspector) ObjectInspectorUtils.getStandardObjectInspector(arguments[0]);
+    if (retValInspector.getListElementObjectInspector().getCategory() != Category.PRIMITIVE) {
+      primitiveObjectInspector = (PrimitiveObjectInspector) retValInspector.getListElementObjectInspector();
+    }
     return retValInspector;
   }
+
 }
