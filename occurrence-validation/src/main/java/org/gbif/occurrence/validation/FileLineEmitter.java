216b2c4146235b379481d2874defd9fc48c52f70
Federico Mendez <fmendez@gbif.org>
2016-09-19 14:50:15 +0200

    reading simple file

--- occurrence-validation/src/main/java/org/gbif/occurrence/validation/FileLineEmitter.java
+++ occurrence-validation/src/main/java/org/gbif/occurrence/validation/FileLineEmitter.java
@@ -1,6 +1,7 @@
 package org.gbif.occurrence.validation;
 
 import org.gbif.api.model.occurrence.VerbatimOccurrence;
+import org.gbif.api.vocabulary.OccurrenceIssue;
 import org.gbif.dwc.terms.Term;
 import org.gbif.dwc.terms.TermFactory;
 import org.gbif.occurrence.processor.interpreting.OccurrenceInterpreter;
@@ -11,9 +12,11 @@ import org.gbif.utils.file.tabular.TabularFiles;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Map;
 
 import akka.actor.UntypedActor;
+import com.google.common.base.Charsets;
 
 public class FileLineEmitter extends UntypedActor {
 
@@ -40,11 +43,12 @@ public class FileLineEmitter extends UntypedActor {
 
     //TODO C.G. provide static utility to get instance of MappedTabularDataFileReader
     try (MappedTabularDataFileReader<Term> mappedTabularFileReader = new MappedTabularDataFileReader(
-            TabularFiles.newTabularFileReader(new FileInputStream(new File(dataInputFile.getFileName())), '"',
-            dataInputFile.getDelimiterChar(), true), columnsMapping)) {
+      TabularFiles.newTabularFileReader(new FileInputStream(new File(dataInputFile.getFileName())),
+                                        dataInputFile.getDelimiterChar(), true), columnsMapping)) {
       Map<Term, String> line;
       while ((line = mappedTabularFileReader.read()) != null) {
         OccurrenceInterpretationResult result = interpreter.interpret(toVerbatimOccurrence(line));
+        result.getUpdated().addIssue(OccurrenceIssue.BASIS_OF_RECORD_INVALID);
         getSender().tell(result);
       }
     }
