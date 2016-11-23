9988ec09a208b13fc595eb83061243875182900e
Christian Gendreau <cgendreau@gbif.org>
2016-11-23 17:00:00 +0100

    new OccurrenceRegistryMapper (wip)

--- occurrence-registry-sync/src/main/java/org/gbif/occurrence/cli/registry/sync/OccurrenceScanMapper.java
+++ occurrence-registry-sync/src/main/java/org/gbif/occurrence/cli/registry/sync/OccurrenceScanMapper.java
@@ -3,43 +3,20 @@ package org.gbif.occurrence.cli.registry.sync;
 import org.gbif.api.model.occurrence.Occurrence;
 import org.gbif.api.model.registry.Dataset;
 import org.gbif.api.model.registry.Organization;
-import org.gbif.api.service.registry.DatasetService;
-import org.gbif.api.service.registry.OrganizationService;
-import org.gbif.common.messaging.api.MessagePublisher;
 import org.gbif.common.messaging.api.messages.DeleteDatasetOccurrencesMessage;
 import org.gbif.common.messaging.api.messages.OccurrenceDeletionReason;
 import org.gbif.common.messaging.api.messages.OccurrenceMutatedMessage;
-import org.gbif.common.messaging.guice.PostalServiceModule;
-import org.gbif.occurrence.cli.registry.RegistryObjectMapperContextResolver;
-import org.gbif.occurrence.common.config.OccHBaseConfiguration;
-import org.gbif.occurrence.persistence.api.OccurrencePersistenceService;
-import org.gbif.occurrence.persistence.guice.OccurrencePersistenceModule;
-import org.gbif.registry.ws.client.DatasetWsClient;
-import org.gbif.registry.ws.client.OrganizationWsClient;
-import org.gbif.ws.mixin.Mixins;
 
 import java.io.IOException;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Set;
 import java.util.UUID;
 
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
-import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.WebResource;
-import com.sun.jersey.api.client.config.ClientConfig;
-import com.sun.jersey.api.client.config.DefaultClientConfig;
-import com.sun.jersey.client.apache.ApacheHttpClient;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
-import org.apache.hadoop.hbase.mapreduce.TableMapper;
 import org.apache.hadoop.hbase.util.Bytes;
-import org.apache.hadoop.io.NullWritable;
-import org.codehaus.jackson.jaxrs.JacksonJsonProvider;
-import org.codehaus.jackson.map.DeserializationConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -49,7 +26,7 @@ import org.slf4j.LoggerFactory;
  * to the occurrence HBase table via occurrence persistence, and then an OccurrenceMutatedMessage is sent. For dataset
  * deletions a DeleteDatasetMessage is sent.
  */
-public class OccurrenceScanMapper extends TableMapper<ImmutableBytesWritable, NullWritable> {
+public class OccurrenceScanMapper extends AbstractOccurrenceRegistryMapper {
 
   private static final Logger LOG = LoggerFactory.getLogger(OccurrenceScanMapper.class);
 
@@ -57,61 +34,7 @@ public class OccurrenceScanMapper extends TableMapper<ImmutableBytesWritable, Nu
   private static final Set<UUID> UNCHANGED_DATASETS = Sets.newHashSet();
   private static final Map<UUID, Organization> DATASET_TO_OWNING_ORG = Maps.newHashMap();
 
-  private DatasetService datasetService;
-  private OrganizationService orgService;
-  private RegistryBasedOccurrenceMutator occurrenceMutator;
-  private OccurrencePersistenceService occurrencePersistenceService;
-  private MessagePublisher messagePublisher;
-  private Client httpClient;
-
   private int numRecords = 0;
-
-  @Override
-  protected void setup(Context context) throws IOException, InterruptedException {
-    super.setup(context);
-    Properties props = new Properties();
-    // extract the config properties from the job context
-    for (Map.Entry<String, String> entry : context.getConfiguration()) {
-      props.setProperty(entry.getKey(), entry.getValue());
-    }
-
-    ClientConfig cc = new DefaultClientConfig();
-    cc.getClasses().add(JacksonJsonProvider.class);
-    cc.getClasses().add(RegistryObjectMapperContextResolver.class);
-    cc.getFeatures().put(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES.toString(), false);
-    RegistryObjectMapperContextResolver.addMixIns(Mixins.getPredefinedMixins());
-
-    httpClient = ApacheHttpClient.create(cc);
-    WebResource regResource = httpClient.resource(props.getProperty(SyncCommon.REG_WS_PROPS_KEY));
-    datasetService = new DatasetWsClient(regResource, null);
-    orgService = new OrganizationWsClient(regResource, null);
-    occurrenceMutator = new RegistryBasedOccurrenceMutator();
-
-    OccHBaseConfiguration occHBaseConfiguration = new OccHBaseConfiguration();
-    occHBaseConfiguration.occTable = props.getProperty("occurrence.db.table_name");
-    occHBaseConfiguration.zkConnectionString = props.getProperty("occurrence.db.zookeeper.connection_string");
-
-    Injector injector =
-      Guice.createInjector(new PostalServiceModule("sync", props),
-              new OccurrencePersistenceModule(occHBaseConfiguration, context.getConfiguration()));
-    occurrencePersistenceService = injector.getInstance(OccurrencePersistenceService.class);
-    messagePublisher = injector.getInstance(MessagePublisher.class);
-  }
-
-  @Override
-  protected void cleanup(Context context) throws IOException, InterruptedException {
-    if (messagePublisher != null) {
-      // wrapped to ensure we also close the httpClient if messagePublisher throws an exception (including runtime)
-      try {
-        messagePublisher.close();
-      }
-      catch (Exception ignore) {}
-    }
-
-    if (httpClient != null) {
-      httpClient.destroy();
-    }
-  }
 
   @Override
   public void map(ImmutableBytesWritable row, Result values, Context context) throws IOException {
