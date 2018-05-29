1aeda3efeba507a0d48a4fdd4ea186b481d12a9d
Matthew Blissett <mblissett@gbif.org>
2018-05-29 17:05:08 +0200

    Add unimplemented methods.

--- occurrence-ws-client/src/test/java/org/gbif/occurrence/ws/client/mock/OccurrenceDownloadMockServices.java
+++ occurrence-ws-client/src/test/java/org/gbif/occurrence/ws/client/mock/OccurrenceDownloadMockServices.java
@@ -5,8 +5,12 @@ import org.gbif.api.model.common.paging.PagingResponse;
 import org.gbif.api.model.occurrence.Download;
 import org.gbif.api.model.registry.DatasetOccurrenceDownloadUsage;
 import org.gbif.api.service.registry.OccurrenceDownloadService;
+import org.gbif.api.vocabulary.Country;
 
+import java.util.Date;
+import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
 
@@ -49,6 +53,22 @@ public class OccurrenceDownloadMockServices implements OccurrenceDownloadService
   @Override
   public PagingResponse<DatasetOccurrenceDownloadUsage> listDatasetUsages(
     @NotNull String s, @Nullable Pageable pageable
+  ) {
+    // TODO: Write implementation
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+
+  @Override
+  public Map<Integer, Map<Integer, Long>> getMonthlyStats(
+    @Nullable Date date, @Nullable Date date1, @Nullable Country country, @Nullable Country country1, @Nullable UUID uuid
+  ) {
+    // TODO: Write implementation
+    throw new UnsupportedOperationException("Not implemented yet");
+  }
+
+  @Override
+  public Map<Integer, Map<Integer, Long>> getDownloadedRecordsStats(
+    @Nullable Date date, @Nullable Date date1, @Nullable Country country, @Nullable Country country1, @Nullable UUID uuid
   ) {
     // TODO: Write implementation
     throw new UnsupportedOperationException("Not implemented yet");
