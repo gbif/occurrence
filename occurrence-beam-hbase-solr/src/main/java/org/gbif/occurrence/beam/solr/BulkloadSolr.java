d770dbfd013f128aa9d285069d65bf91761c1f8a
timrobertson100 <timrobertson100@gmail.com>
2018-07-06 17:14:30 +0200

    WIP on Beam indexing. All working and used in UAT

--- occurrence-beam-hbase-solr/src/main/java/org/gbif/occurrence/beam/solr/BulkloadSolr.java
+++ occurrence-beam-hbase-solr/src/main/java/org/gbif/occurrence/beam/solr/BulkloadSolr.java
@@ -16,50 +16,44 @@ import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.solr.common.SolrInputDocument;
 import org.gbif.api.model.occurrence.Occurrence;
-import org.gbif.occurrence.beam.solr.io.PatchedSolrIO;
 import org.gbif.occurrence.persistence.util.OccurrenceBuilder;
 import org.gbif.occurrence.search.writer.SolrOccurrenceWriter;
+import org.joda.time.Duration;
 
-/**
- * Executes a pipeline that reads HBase and loads SOLR.
- */
+/** Executes a pipeline that reads HBase and loads SOLR. */
 public class BulkloadSolr {
   // TODO - paramaterise this
-  //private static final String HBASE_ZK = "c4zk1.gbif-uat.org,c4zk2.gbif-uat.org,c4zk3.gbif-uat.org";
-  //private static final String HBASE_TABLE = "uat_occurrence";
-  //private static final String SOLR_HOST = "c4zk1.gbif-uat.org,c4zk2.gbif-uat.org,c4zk3.gbif-uat.org/solrp";
-  //private static final String SOLR_COLLECTION = "tim-occurrence";
-
-
-  private static final String HBASE_ZK = "c5zk1.gbif.org,c5zk2.gbif.org,c5zk3.gbif.org";
-  private static final String HBASE_TABLE = "prod_e_occurrence";
-  private static final String SOLR_HOST = "c5zk1.gbif.org,c5zk2.gbif.org,c5zk3.gbif.org/solr5";
+  private static final String HBASE_ZK = "c4zk1.gbif-uat.org,c4zk2.gbif-uat.org,c4zk3.gbif-uat.org";
+  private static final String HBASE_TABLE = "uat_occurrence";
+  private static final String SOLR_HOST =
+      "c4zk1.gbif-uat.org,c4zk2.gbif-uat.org,c4zk3.gbif-uat.org/solr5";
   private static final String SOLR_COLLECTION = "tim-occurrence";
+
+  // private static final String HBASE_ZK = "c5zk1.gbif.org,c5zk2.gbif.org,c5zk3.gbif.org";
+  // private static final String HBASE_TABLE = "prod_e_occurrence";
+  // private static final String SOLR_HOST = "c5zk1.gbif.org,c5zk2.gbif.org,c5zk3.gbif.org/solr5";
+  // private static final String SOLR_COLLECTION = "tim-occurrence";
 
   public static void main(String[] args) {
     PipelineOptions options = PipelineOptionsFactory.create();
     options.setRunner(SparkRunner.class);
     Pipeline p = Pipeline.create(options);
 
-
     Configuration hbaseConfig = HBaseConfiguration.create();
     hbaseConfig.set("hbase.zookeeper.quorum", HBASE_ZK);
 
     Scan scan = new Scan();
-    scan.setBatch(1000); // for safety
+    scan.setBatch(10000); // for safety
     scan.addFamily("o".getBytes());
 
     PCollection<Result> rows =
         p.apply(
             "read",
-            HBaseIO.read()
-                .withConfiguration(hbaseConfig)
-                .withScan(scan)
-                .withTableId(HBASE_TABLE));
+            HBaseIO.read().withConfiguration(hbaseConfig).withScan(scan).withTableId(HBASE_TABLE));
 
     PCollection<SolrInputDocument> docs =
         rows.apply(
-            "convert to SOLR docs",
+            "convert",
             ParDo.of(
                 new DoFn<Result, SolrInputDocument>() {
 
@@ -69,7 +63,8 @@ public class BulkloadSolr {
                     try {
                       Occurrence occurrence = OccurrenceBuilder.buildOccurrence(row);
 
-                      SolrInputDocument document = SolrOccurrenceWriter.buildOccSolrDocument(occurrence);
+                      SolrInputDocument document =
+                          SolrOccurrenceWriter.buildOccSolrDocument(occurrence);
                       c.output(document);
 
                     } catch (NullPointerException e) {
@@ -78,11 +73,16 @@ public class BulkloadSolr {
                   }
                 }));
 
-      final PatchedSolrIO.ConnectionConfiguration conn = PatchedSolrIO.ConnectionConfiguration.create(SOLR_HOST);
+    final SolrIO.ConnectionConfiguration conn = SolrIO.ConnectionConfiguration.create(SOLR_HOST);
 
-      docs.apply(PatchedSolrIO.write().to(SOLR_COLLECTION).withConnectionConfiguration(conn));
+    docs.apply("write",
+        SolrIO.write()
+            .to(SOLR_COLLECTION)
+            .withConnectionConfiguration(conn)
+            .withRetryConfiguration(
+                SolrIO.RetryConfiguration.create(10000, Duration.standardMinutes(5))));
 
-      PipelineResult result = p.run();
-      result.waitUntilFinish();
+    PipelineResult result = p.run();
+    result.waitUntilFinish();
   }
 }
