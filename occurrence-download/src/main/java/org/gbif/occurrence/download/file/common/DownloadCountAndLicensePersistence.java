842bcaf5adf978f858d79fa2c854ac61e3297cdc
aalbatross <rpathak@gbif.org>
2018-11-16 16:20:34 +0100

    #71 updating license information for sql downloads

--- occurrence-download/src/main/java/org/gbif/occurrence/download/file/common/DownloadCountAndLicensePersistence.java
+++ occurrence-download/src/main/java/org/gbif/occurrence/download/file/common/DownloadCountAndLicensePersistence.java
@@ -2,7 +2,9 @@ package org.gbif.occurrence.download.file.common;
 
 import java.io.IOException;
 import java.util.Properties;
+import org.gbif.api.model.occurrence.Download;
 import org.gbif.api.service.registry.OccurrenceDownloadService;
+import org.gbif.api.vocabulary.License;
 import org.gbif.occurrence.download.inject.DownloadWorkflowModule;
 import org.gbif.occurrence.download.util.RegistryClientUtil;
 import org.gbif.utils.file.properties.PropertiesUtil;
@@ -10,14 +12,15 @@ import com.google.common.base.Preconditions;
 
 /**
  * 
- * Oozie action persists occurrence download count information to registry.
+ * Oozie action persists occurrence downloads count information and license to registry.
  *
  */
-public class DownloadCountPersistence {
+public class DownloadCountAndLicensePersistence {
 
   public static void main(String[] args) throws IOException{
     String countPath = Preconditions.checkNotNull(args[0]);
     String downloadKey = Preconditions.checkNotNull(args[1]);
+    String license = Preconditions.checkNotNull(args[2]);
     
     Properties properties = PropertiesUtil.loadProperties(DownloadWorkflowModule.CONF_FILE);
     String nameNode = properties.getProperty(DownloadWorkflowModule.DefaultSettings.NAME_NODE_KEY);
@@ -27,5 +30,8 @@ public class DownloadCountPersistence {
     OccurrenceDownloadService occurrenceDownloadService = registryClientUtil.setupOccurrenceDownloadService(registryWsURL);
     // persists species count information.
     DownloadCount.persist(downloadKey, DownloadFileUtils.readCount(nameNode, countPath), occurrenceDownloadService);
+    Download download = occurrenceDownloadService.get(downloadKey);
+    download.setLicense(License.valueOf(license));
+    occurrenceDownloadService.update(download);
   }
 }
