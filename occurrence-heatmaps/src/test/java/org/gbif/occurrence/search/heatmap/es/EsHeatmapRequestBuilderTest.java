2f0aadbd58a330702f6bdee7ecfa9f651867b7df
fmendezh <federicomh@gmail.com>
2019-03-26 13:24:07 +0100

    fixing heatmaps tests, it assumed that the order of filter was fixed when in reality the order can change

--- occurrence-heatmaps/src/test/java/org/gbif/occurrence/search/heatmap/es/EsHeatmapRequestBuilderTest.java
+++ occurrence-heatmaps/src/test/java/org/gbif/occurrence/search/heatmap/es/EsHeatmapRequestBuilderTest.java
@@ -2,18 +2,28 @@ package org.gbif.occurrence.search.heatmap.es;
 
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.node.ArrayNode;
 import org.elasticsearch.action.search.SearchRequest;
 import org.gbif.occurrence.search.es.OccurrenceEsField;
 import org.gbif.occurrence.search.heatmap.OccurrenceHeatmapRequest;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Optional;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.StreamSupport;
 
 import static org.gbif.occurrence.search.es.EsQueryUtils.*;
 import static org.gbif.occurrence.search.heatmap.es.EsHeatmapRequestBuilder.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+/**
+ * Tests for ElasticSearch heatmap request builders.
+ */
 public class EsHeatmapRequestBuilderTest {
 
   private static final ObjectMapper MAPPER = new ObjectMapper();
@@ -27,7 +37,6 @@ public class EsHeatmapRequestBuilderTest {
 
     SearchRequest query = EsHeatmapRequestBuilder.buildRequest(request, INDEX);
     JsonNode json = MAPPER.readTree(query.source().toString());
-    System.out.println(json.toString());
 
     assertEquals(0, json.get(SIZE).asInt());
 
@@ -84,6 +93,17 @@ public class EsHeatmapRequestBuilderTest {
         OccurrenceEsField.COORDINATE_POINT.getFieldName(), jsonGeobounds.get(FIELD).asText());
   }
 
+  /**
+   * Tries to find a field in the list of term filters.
+   */
+  private static Optional<String> findTermFilter(JsonNode node, OccurrenceEsField field) {
+    ArrayNode arrayNode = (ArrayNode)node.path(QUERY).path(BOOL).path(FILTER);
+    return StreamSupport.stream(Spliterators.spliterator(arrayNode.getElements(), 2, Spliterator.ORDERED), false)
+              .filter(termNode -> termNode.path(TERM).has(field.getFieldName()))
+              .map(termNode -> termNode.path(TERM).get(field.getFieldName()).get(VALUE).asText())
+              .findFirst();
+  }
+
   @Test
   public void heatmapRequestFilteredTest() throws IOException {
     OccurrenceHeatmapRequest request = new OccurrenceHeatmapRequest();
@@ -93,23 +113,21 @@ public class EsHeatmapRequestBuilderTest {
 
     SearchRequest query = EsHeatmapRequestBuilder.buildRequest(request, INDEX);
     JsonNode json = MAPPER.readTree(query.source().toString());
-    System.out.println(json.toString());
 
     assertEquals(0, json.get(SIZE).asInt());
     assertTrue(json.path(QUERY).path(BOOL).path(FILTER).isArray());
     assertTrue(json.path(QUERY).path(BOOL).path(FILTER).get(0).has(TERM));
 
     // taxon key
-    assertEquals(
-        4,
-        json.path(QUERY)
-            .path(BOOL)
-            .path(FILTER)
-            .get(0)
-            .path(TERM)
-            .get(OccurrenceEsField.TAXA_KEY.getFieldName())
-            .get(VALUE)
-            .asInt());
+    Optional<String> taxaValue = findTermFilter(json, OccurrenceEsField.TAXA_KEY);
+
+    if (taxaValue.isPresent()) {
+      assertEquals(4, Integer.parseInt(taxaValue.get()));
+    } else {
+      Assert.fail("TaxaKey term not found");
+    }
+
+
 
     // aggs
     assertTrue(json.path(AGGREGATIONS).path(BOX_AGGS).path(FILTER).has(GEO_BOUNDING_BOX));
