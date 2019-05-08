2e7041cee4b7284216c4eb1f9bf76e069f1381df
fmendezh <federicomh@gmail.com>
2019-05-08 16:36:55 +0200

    exposing a searchOnEngine method that later can be used to execute queries using the ElasticSearch api to consume straight ES responses

--- occurrence-heatmaps/src/main/java/org/gbif/occurrence/search/heatmap/OccurrenceHeatmapService.java
+++ occurrence-heatmaps/src/main/java/org/gbif/occurrence/search/heatmap/OccurrenceHeatmapService.java
@@ -6,8 +6,10 @@ import javax.annotation.Nullable;
 
 /**
  * Generic interface for Heatmap services.
+ * @param <S> search engine search request type
+ * @param <R> search engine search response type
  */
-public interface OccurrenceHeatmapService {
+public interface OccurrenceHeatmapService<S,R> {
 
   /**
    * Provides a HeatMap aggregation based on GeoBounds.
@@ -18,4 +20,15 @@ public interface OccurrenceHeatmapService {
    * Provides a HeatMap aggregation based on a GeoCentroid.
    */
   EsOccurrenceHeatmapResponse.GeoCentroidResponse searchHeatMapGeoCentroid(@Nullable OccurrenceHeatmapRequest request);
+
+
+  /**
+   * Performs a search using the request and response types supported by the SearchEngine.
+   * This method is used to perform 'native' queries on the SearchEngine (ElasticSearch, Solr, etc).
+   * @param searchRequest query
+   * @param <S> search engine search request type
+   * @param <R> search engine search response type
+   * @return and search engine response
+   */
+   R searchOnEngine(S searchRequest);
 }
