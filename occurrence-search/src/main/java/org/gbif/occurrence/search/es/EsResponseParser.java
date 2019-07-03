d6419057f723fa1452d8876aa61bb7204597ccf4
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2019-07-03 14:07:30 +0200

    updated default shard size for ES aggs to improve accuracy in results

--- occurrence-search/src/main/java/org/gbif/occurrence/search/es/EsResponseParser.java
+++ occurrence-search/src/main/java/org/gbif/occurrence/search/es/EsResponseParser.java
@@ -139,7 +139,7 @@ public class EsResponseParser {
 
                           UnaryOperator<String> valueNormalizer =
                               v ->
-                                  Country.class.isAssignableFrom(facet.type()) ? v.toUpperCase() : v;
+                                  Enum.class.isAssignableFrom(facet.type()) ? v.toUpperCase() : v;
 
                           List<Facet.Count> counts =
                               buckets.stream()
