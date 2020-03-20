1339e03cfa7c18fc05fe51e359cb2f7014b74827
fmendezh <federicomh@gmail.com>
2020-03-20 10:49:15 +0100

    copying directoy content

--- occurrence-download/run-workflow-schema-migration.sh
+++ occurrence-download/run-workflow-schema-migration.sh
@@ -24,7 +24,7 @@ java -classpath "target/occurrence-download-workflows-$ENV/lib/*" org.gbif.occur
 echo "Copy to hadoop"
 sudo -u hdfs hdfs dfs -rm -r -f /occurrence-download-workflows-new-schema-$ENV/
 sudo -u hdfs hdfs dfs -mkdir  /occurrence-download-workflows-new-schema-$ENV/
-sudo -u hdfs hdfs dfs -copyFromLocal target/occurrence-download-workflows-$ENV/  /occurrence-download-workflows-new-schema-$ENV/
+sudo -u hdfs hdfs dfs -copyFromLocal target/occurrence-download-workflows-$ENV/*  /occurrence-download-workflows-new-schema-$ENV/
 echo -e "oozie.use.system.libpath=true\noozie.launcher.mapreduce.user.classpath.first=true\noozie.wf.application.path=$NAME_NODE/occurrence-download-workflows-new-schema-$ENV/create-tables\nhiveDB=$HIVE_DB\noozie.libpath=/occurrence-download-workflows-new-schema-$ENV/lib/,/user/oozie/share/lib/gbif/hive\noozie.launcher.mapreduce.task.classpath.user.precedence=true\nuser.name=hdfs\nenv=$ENV\nsource_data_dir=hdfs://ha-nn/data/hdfsview/occurrence/\n=download_table_name=occurrence_new"  > job.properties
 
 sudo -u hdfs oozie job --oozie $OOZIE -config job.properties -run
