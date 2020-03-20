75467c261ad2092c96d1e64146b34d45a1512ed0
fmendezh <federicomh@gmail.com>
2020-03-20 11:07:27 +0100

    allowing parameters with default values for the source directory and table name

--- occurrence-download/run-workflow-schema-migration.sh
+++ occurrence-download/run-workflow-schema-migration.sh
@@ -6,6 +6,9 @@ set -o pipefail
 
 P=$1
 TOKEN=$2
+SOURCE_DIR=${3:-hdfs://ha-nn/data/hdfsview/occurrence/}
+TABLE_NAME=${4:-occurrence_new}
+
 
 echo "Get latest tables-coord config profiles from github"
 curl -s -H "Authorization: token $TOKEN" -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/gbif/gbif-configuration/contents/occurrence-download/profiles.xml
@@ -25,6 +28,6 @@ echo "Copy to hadoop"
 sudo -u hdfs hdfs dfs -rm -r -f /occurrence-download-workflows-new-schema-$ENV/
 sudo -u hdfs hdfs dfs -mkdir  /occurrence-download-workflows-new-schema-$ENV/
 sudo -u hdfs hdfs dfs -copyFromLocal target/occurrence-download-workflows-$ENV/*  /occurrence-download-workflows-new-schema-$ENV/
-echo -e "oozie.use.system.libpath=true\noozie.launcher.mapreduce.user.classpath.first=true\noozie.wf.application.path=$NAME_NODE/occurrence-download-workflows-new-schema-$ENV/create-tables\nhiveDB=$HIVE_DB\noozie.libpath=/occurrence-download-workflows-new-schema-$ENV/lib/,/user/oozie/share/lib/gbif/hive\noozie.launcher.mapreduce.task.classpath.user.precedence=true\nuser.name=hdfs\nenv=$ENV\nsource_data_dir=hdfs://ha-nn/data/hdfsview/occurrence/\ndownload_table_name=occurrence_new"  > job.properties
+echo -e "oozie.use.system.libpath=true\noozie.launcher.mapreduce.user.classpath.first=true\noozie.wf.application.path=$NAME_NODE/occurrence-download-workflows-new-schema-$ENV/create-tables\nhiveDB=$HIVE_DB\noozie.libpath=/occurrence-download-workflows-new-schema-$ENV/lib/,/user/oozie/share/lib/gbif/hive\noozie.launcher.mapreduce.task.classpath.user.precedence=true\nuser.name=hdfs\nenv=$ENV\nsource_data_dir=$SOURCE_DIR\ndownload_table_name=$TABLE_NAME"  > job.properties
 
 sudo -u hdfs oozie job --oozie $OOZIE -config job.properties -run
