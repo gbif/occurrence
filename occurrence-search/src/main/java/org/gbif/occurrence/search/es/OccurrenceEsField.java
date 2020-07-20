cd2c73f493a7077aad40b7d95f7f0d78560cda39
Nikolay Volik <nvolik@gbif.org>
2020-07-20 15:48:16 +0200

    gbif/pipelines#268
    Added new values for occurrence status search/downloads

--- occurrence-search/src/main/java/org/gbif/occurrence/search/es/OccurrenceEsField.java
+++ occurrence-search/src/main/java/org/gbif/occurrence/search/es/OccurrenceEsField.java
@@ -36,6 +36,7 @@ public enum OccurrenceEsField {
   RECORD_NUMBER("recordNumber", DwcTerm.recordNumber),
   BASIS_OF_RECORD("basisOfRecord", DwcTerm.basisOfRecord),
   TYPE_STATUS("typeStatus", DwcTerm.typeStatus),
+  OCCURRENCE_STATUS("occurrenceStatus", DwcTerm.occurrenceStatus),
 
   //Temporal
   YEAR("year", DwcTerm.year),
