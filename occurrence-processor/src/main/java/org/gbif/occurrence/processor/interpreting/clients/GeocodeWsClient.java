1c69c753645d00e275b17e6b86553686bc6434b5
fmendezh <federicomh@gmail.com>
2020-11-27 10:56:06 +0100

    upgrading to latest gbif versions

--- occurrence-processor/src/main/java/org/gbif/occurrence/processor/interpreting/clients/GeocodeWsClient.java
+++ occurrence-processor/src/main/java/org/gbif/occurrence/processor/interpreting/clients/GeocodeWsClient.java
@@ -4,6 +4,7 @@ import org.gbif.geocode.api.model.Location;
 import org.gbif.geocode.api.service.GeocodeService;
 
 import java.util.Collection;
+import java.util.List;
 
 import javax.annotation.Nullable;
 
@@ -25,7 +26,19 @@ public interface GeocodeWsClient extends GeocodeService {
   )
   @ResponseBody
   @Override
-  Collection<Location> get(@RequestParam("lat") Double lat, @RequestParam("lng") Double lng,@Nullable @RequestParam("uncertainty") Double uncertainty);
+  Collection<Location> get(@RequestParam("lat") Double lat, @RequestParam("lng") Double lng,
+                           @Nullable @RequestParam(value = "uncertaintyDegrees", required = false) Double uncertaintyDegrees,
+                           @Nullable @RequestParam(value = "uncertaintyMeters", required = false) Double uncertaintyMeters);
+
+  @RequestMapping(
+    method = RequestMethod.GET
+  )
+  @ResponseBody
+  @Override
+  Collection<Location> get(@RequestParam("lat") Double lat, @RequestParam("lng") Double lng,
+                                  @Nullable @RequestParam(value = "uncertaintyDegrees", required = false) Double uncertaintyDegrees,
+                                  @Nullable @RequestParam(value = "uncertaintyMeters", required = false) Double uncertaintyMeters,
+                                  @Nullable @RequestParam(value = "layer", required = false) List<String> layers);
 
   @RequestMapping(
     method = RequestMethod.GET,
@@ -34,4 +47,6 @@ public interface GeocodeWsClient extends GeocodeService {
   @ResponseBody
   @Override
   byte[] bitmap();
+
+
 }
