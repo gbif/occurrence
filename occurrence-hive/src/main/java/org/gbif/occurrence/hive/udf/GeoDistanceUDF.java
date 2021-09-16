f0314034a4ae131c8b70c0b35c7d5525c8ced342
fmendezh <federicomh@gmail.com>
2021-09-16 09:27:47 +0200

    Using Lucene SloppyMath to calculate the geo distance which should provide results similar to Elasticsearch

--- occurrence-hive/src/main/java/org/gbif/occurrence/hive/udf/GeoDistanceUDF.java
+++ occurrence-hive/src/main/java/org/gbif/occurrence/hive/udf/GeoDistanceUDF.java
@@ -2,20 +2,11 @@ package org.gbif.occurrence.hive.udf;
 
 import org.gbif.api.model.occurrence.geo.DistanceUnit;
 
-import java.util.Map;
-
-import com.google.common.collect.Maps;
 import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.BooleanWritable;
-import org.locationtech.jts.geom.Geometry;
-import org.locationtech.spatial4j.context.jts.DatelineRule;
-import org.locationtech.spatial4j.context.jts.JtsSpatialContext;
-import org.locationtech.spatial4j.context.jts.JtsSpatialContextFactory;
-import org.locationtech.spatial4j.distance.DistanceUtils;
+import org.apache.lucene.util.SloppyMath;
 import org.locationtech.spatial4j.exception.InvalidShapeException;
-import org.locationtech.spatial4j.io.WKTReader;
-import org.locationtech.spatial4j.shape.jts.JtsShapeFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,20 +18,7 @@ public class GeoDistanceUDF extends UDF {
 
   private static final Logger LOG = LoggerFactory.getLogger(GeoDistanceUDF.class);
 
-  private final Map<String, Geometry> geometryCache = Maps.newHashMap();
   private final BooleanWritable isContained = new BooleanWritable();
-
-  private final JtsShapeFactory shapeFactory;
-
-  public GeoDistanceUDF() {
-    JtsSpatialContextFactory spatialContextFactory = new JtsSpatialContextFactory();
-    spatialContextFactory.normWrapLongitude = true;
-    spatialContextFactory.srid = 4326;
-    // NB the “Rect” here: large polygons with ≥180° between longitudinal points will be processed incorrectly.
-    spatialContextFactory.datelineRule = DatelineRule.ccwRect;
-
-    shapeFactory = new JtsShapeFactory(spatialContextFactory.newSpatialContext(), spatialContextFactory);
-  }
 
   public BooleanWritable evaluate(Double latitudeCentroid, Double longitudeCentroid, String distance, Double latitude, Double longitude) {
     isContained.set(false);
@@ -51,22 +29,16 @@ public class GeoDistanceUDF extends UDF {
         || longitude > 180 || longitude < -180) {
         return isContained;
       }
-      DistanceUnit.GeoDistance geoDistance = new DistanceUnit.GeoDistance(latitudeCentroid, longitudeCentroid, DistanceUnit.parseDistance(distance));
+      DistanceUnit.GeoDistance geoDistance = new DistanceUnit.GeoDistance(latitudeCentroid,
+                                                                          longitudeCentroid,
+                                                                          DistanceUnit.parseDistance(distance));
 
-      Geometry geom = geometryCache.get(geoDistance.toGeoDistanceString());
-      if (geom == null) {
-        geom = JtsSpatialContext.GEO.getShapeFactory().getGeometryFrom(
-        shapeFactory.circle(longitudeCentroid, latitudeCentroid,
-                            DistanceUtils.dist2Degrees(DistanceUnit.convert(geoDistance.getDistance().getValue(),
-                                                                            geoDistance.getDistance().getUnit(), DistanceUnit.KILOMETERS),
-                                                       DistanceUtils.EARTH_MEAN_RADIUS_KM)));
-        geometryCache.put(geoDistance.toGeoDistanceString(), geom);
-      }
+      double haversinDistance = SloppyMath.haversinMeters(latitudeCentroid, longitudeCentroid, latitude, longitude);
+      double distanceInMeters = DistanceUnit.convert(geoDistance.getDistance().getValue(),
+                                                     geoDistance.getDistance().getUnit(),
+                                                     DistanceUnit.METERS);
 
-      // support any geometry - up to the user to make a sensible query
-      Geometry point = JtsSpatialContext.GEO.getShapeFactory().getGeometryFrom(shapeFactory.pointXY(longitude, latitude));
-
-      isContained.set(geom.intersects(point));
+      isContained.set(haversinDistance <= distanceInMeters);
 
     } catch (InvalidShapeException e) {
       LOG.error("Invalid geometry received", e);
---
+++ occurrence-hive/src/test/java/org/gbif/occurrence/hive/udf/GeoDistanceUDFTest.java
@@ -0,0 +1,29 @@
+package org.gbif.occurrence.hive.udf;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class GeoDistanceUDFTest {
+
+  @Test
+  public void evaluateTest() {
+
+    // At the Equator, moving 0.04° is 4448m.
+    //assertTrue(contains(0d, 0d, "5km", 0d, 0.04d));
+    // And 0.5° is 5560m.
+    assertFalse(contains(0d, 0d, "5km", 0d, 0.05d));
+
+    // At 80° north, moving 0.04° is only 772m.
+    assertTrue(contains(80d, 0d, "5km", 80d, 0.04d));
+    // But we can move 0.25° and still have moved only 4827m.
+    assertTrue(contains(80d, 0d, "5km", 80d, 0.25d));
+    // This is 5020m.
+    assertFalse(contains(80d, 0d, "5km", 80d, 0.26d));
+  }
+
+  private boolean contains(Double latitude1, Double longitude1, String distance, Double latitude2, Double longitude2) {
+    return new GeoDistanceUDF().evaluate(latitude1, longitude1, distance, latitude2, longitude2).get();
+  }
+}
