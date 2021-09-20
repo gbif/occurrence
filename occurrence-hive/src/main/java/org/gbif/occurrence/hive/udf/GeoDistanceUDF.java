2dd788013ceb90f6f0fb7515fa89ab016be1cc6a
fmendezh <federicomh@gmail.com>
2021-09-20 16:11:07 +0200

    removing unnecessary distance conversion and moving steps to simple functions

--- occurrence-hive/src/main/java/org/gbif/occurrence/hive/udf/GeoDistanceUDF.java
+++ occurrence-hive/src/main/java/org/gbif/occurrence/hive/udf/GeoDistanceUDF.java
@@ -6,48 +6,51 @@ import org.apache.hadoop.hive.ql.exec.Description;
 import org.apache.hadoop.hive.ql.exec.UDF;
 import org.apache.hadoop.io.BooleanWritable;
 import org.apache.lucene.util.SloppyMath;
-import org.locationtech.spatial4j.exception.InvalidShapeException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * A simple UDF for Hive to test if a coordinate is within a given distance of a geopoint.
+ * A simple UDF for Hive to test if two coordinates are in a given distance.
  */
 @Description(name = "geo_distance", value = "_FUNC_(centroidLatitude, centroidLongitude, distance, latitude, longitude)")
 public class GeoDistanceUDF extends UDF {
 
   private static final Logger LOG = LoggerFactory.getLogger(GeoDistanceUDF.class);
 
-  private final BooleanWritable isContained = new BooleanWritable();
+  private final BooleanWritable isInDistance = new BooleanWritable();
 
-  public BooleanWritable evaluate(Double latitudeCentroid, Double longitudeCentroid, String distance, Double latitude, Double longitude) {
-    isContained.set(false);
+  public BooleanWritable evaluate(Double lat1, Double lon1, String distance, Double lat2, Double lon2) {
+    isInDistance.set(false);
 
     try {
       // sanitize the input
-      if (latitudeCentroid == null || longitudeCentroid == null || distance == null || latitude == null || longitude == null || latitude > 90 || latitude < -90
-        || longitude > 180 || longitude < -180) {
-        return isContained;
+      if (lat1 == null || lon1 == null || distance == null || lat2 == null || lon2 == null || lat2 > 90 || lat2 < -90
+        || lon2 > 180 || lon2 < -180) {
+        return isInDistance;
       }
-      DistanceUnit.GeoDistance geoDistance = new DistanceUnit.GeoDistance(latitudeCentroid,
-                                                                          longitudeCentroid,
-                                                                          DistanceUnit.parseDistance(distance));
 
-      double haversinDistance = SloppyMath.haversinMeters(latitudeCentroid, longitudeCentroid, latitude, longitude);
-      double distanceInMeters = DistanceUnit.convert(geoDistance.getDistance().getValue(),
-                                                     geoDistance.getDistance().getUnit(),
-                                                     DistanceUnit.METERS);
+      isInDistance.set(areInHaversineDistance(lat1, lon1, distance, lat2, lon2));
 
-      isContained.set(haversinDistance <= distanceInMeters);
-
-    } catch (InvalidShapeException e) {
-      LOG.error("Invalid geometry received", e);
-      throw new RuntimeException(e);
     } catch (Exception e) {
       LOG.error("Error applying UDF", e);
       throw new RuntimeException(e);
     }
+    return isInDistance;
+  }
 
-    return isContained;
+  /** Are 2 coordinates in haversine distance.*/
+  private static boolean areInHaversineDistance(double lat1, double lon1, String distance, double lat2, double lon2) {
+    return haversineDistance(lat1, lon1, lat2, lon2) <= distanceInMeters(distance);
+  }
+
+  /** Distance in meters, between 2  coordinates using the haversine method implemented by lucene */
+  private static double haversineDistance(double lat1, double lon1, double lat2, double lon2) {
+    return SloppyMath.haversinMeters(lat1, lon1, lat2, lon2);
+  }
+
+  /** Parse and converts a distance to meters.*/
+  private static double distanceInMeters(String distance) {
+    DistanceUnit.Distance geoDistance = DistanceUnit.parseDistance(distance);
+    return DistanceUnit.convert(geoDistance.getValue(), geoDistance.getUnit(), DistanceUnit.METERS);
   }
 }
