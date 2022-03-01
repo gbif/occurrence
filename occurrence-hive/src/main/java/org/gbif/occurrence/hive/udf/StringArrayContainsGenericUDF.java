1005db25d8979503968bfcb145f5bfbc503c1689
Marcos Lopez Gonzalez <asturcon1234@gmail.com>
2022-03-01 10:54:35 +0100

    fix StringArrayContainsGenericUDF

--- occurrence-hive/src/main/java/org/gbif/occurrence/hive/udf/StringArrayContainsGenericUDF.java
+++ occurrence-hive/src/main/java/org/gbif/occurrence/hive/udf/StringArrayContainsGenericUDF.java
@@ -16,7 +16,6 @@ package org.gbif.occurrence.hive.udf;
 import java.util.List;
 
 import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
-import org.apache.hadoop.hive.ql.exec.UDFArgumentTypeException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
 import org.apache.hadoop.hive.serde2.objectinspector.ObjectInspector;
