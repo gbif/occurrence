4bb13e46a43b0dd77ff456e66a82e5112b128fef
fmendezh <federicomh@gmail.com>
2022-08-08 10:21:45 +0200

    Ignoring parentEventGbifId from table schemas

--- occurrence-hdfs-table/src/main/java/org/gbif/occurrence/download/hive/DownloadTerms.java
+++ occurrence-hdfs-table/src/main/java/org/gbif/occurrence/download/hive/DownloadTerms.java
@@ -48,7 +48,8 @@ public class DownloadTerms {
     GbifInternalTerm.fragmentHash, // omitted entirely
     GbifInternalTerm.fragment, // omitted entirely
     GbifTerm.numberOfOccurrences, //this is for aggregation only
-    GbifTerm.eventType //This is an experimental field
+    GbifTerm.eventType, //This is an experimental field
+    GbifInternalTerm.parentEventGbifId //This is an internal field for Event data
   );
 
   // This set is used fot the HDFS table definition
