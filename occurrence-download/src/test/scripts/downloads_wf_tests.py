4731e3271ee2681f85c7fcbbe51690cf739bfa9a
fmendezh <federicomh@gmail.com>
2022-11-16 10:00:42 +0100

    adding messages to sys out for better tracking of progress

--- occurrence-download/src/test/scripts/downloads_wf_tests.py
+++ occurrence-download/src/test/scripts/downloads_wf_tests.py
@@ -2,13 +2,15 @@
   It creates a download for each file, waits for its response, once it finishes runs the tests in download_tests.py
 """
 
-import glob, json, os, requests, time, sys, argparse, subprocess
+import glob, json, os, requests, time, sys, argparse, subprocess, time
 
 # Download finish statuses
 finish_status = ['SUCCEEDED', 'CANCELLED', 'KILLED', 'FAILED', 'SUSPENDED', 'FILE_ERASED']
 
 # environment with postfix names
 env_uat = ['dev', 'uat']
+
+wait_status_time = 10
 
 """ Builds the url to create a download
 """
@@ -19,7 +21,9 @@ def download_request_url(env, download_type):
 """ Triggers the creation of an download
 """
 def post_request_download(download_request, username, password, env, download_type):
-  return requests.post(download_request_url(env, download_type), json=download_request, auth=(username, password)).text
+  request_url = download_request_url(env, download_type)
+  print("Posting download request to url {} using request body \n {}".format(request_url, json.dumps(download_request, indent=2)))
+  return requests.post(request_url, json=download_request, auth=(username, password)).text
 
 
 """ Builds the url to get a download info
@@ -69,16 +73,20 @@ if __name__ == '__main__':
   for file in files:
     download_request_obj = download_request(file, args.username, args.email)
     download_type = download_request_obj.get('type', args.download_type).lower()
+    start_time = time.time()
     download_id = post_request_download(download_request_obj, args.username, args.password, args.env, download_type)
-
+    print("Download request created, download key {}".format(download_id))
     finished = False
     while not finished:
-      time.sleep(10)
+      time.sleep(wait_status_time)
       download_response = get_download(download_id, args.username, args.password, args.env, download_type)
-
+      elapsed_time = time.time() - start_time
       if download_response['status'] in finish_status:
         finished = True
+        print("Download {} finished with status {} in {} seconds".format(download_id, download_response['status'], elapsed_time))
         if download_response['status'] == 'SUCCEEDED':
           run_download_tests(download_id, args.env, args.work_dir, download_type)
         else:
           sys.exit('Download ' + download_id + ' failed')
+      else:
+        print("Download {} not ready after {} seconds, waiting {} seconds more".format(download_id, elapsed_time, wait_status_time))
