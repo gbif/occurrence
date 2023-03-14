9915a649b6191d15bef724756879073dba5834a2
nvolik <nikolay.volik@hotmail.com>
2023-03-14 16:29:14 +0100

    Run Spark job using SparkLauncher

--- occurrence-download-spark/src/main/java/org/gbif/occurrence/YarnJobManager.java
+++ occurrence-download-spark/src/main/java/org/gbif/occurrence/YarnJobManager.java
@@ -1,8 +1,11 @@
 package org.gbif.occurrence;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.EnumSet;
+import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 
@@ -12,16 +15,11 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.yarn.api.records.ApplicationId;
 import org.apache.hadoop.yarn.api.records.ApplicationReport;
-import org.apache.hadoop.yarn.api.records.ApplicationSubmissionContext;
-import org.apache.hadoop.yarn.api.records.ContainerLaunchContext;
 import org.apache.hadoop.yarn.api.records.YarnApplicationState;
 import org.apache.hadoop.yarn.client.api.YarnClient;
-import org.apache.hadoop.yarn.client.api.YarnClientApplication;
 import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.yarn.exceptions.YarnException;
-import org.apache.spark.SparkConf;
-import org.apache.spark.deploy.yarn.Client;
-import org.apache.spark.deploy.yarn.ClientArguments;
+import org.apache.spark.launcher.SparkLauncher;
 
 import javax.validation.constraints.NotNull;
 import lombok.extern.slf4j.Slf4j;
@@ -48,60 +46,28 @@ public class YarnJobManager implements JobManager {
   @Override
   public Optional<String> createJob(@NotNull String jobId, @NotNull DownloadRequest downloadRequest) {
     try {
-      SparkConf sparkConf = new SparkConf().setMaster("yarn").setAppName(jobId).set("deploy-mode", "cluster");
+      new SparkLauncher()
+        .setAppName(jobId)
+        .setSparkHome("/opt/cloudera/parcels/CDH-5.16.2-1.cdh5.16.2.p0.8/lib/spark")
+        .setDeployMode("cluster")
+        .setMaster("yarn")
+        .setAppResource("hdfs://ha-nn/pipelines/jars/ingest-gbif.jar")
+        .setMainClass("org.gbif.pipelines.ingest.pipelines.VerbatimToIdentifierPipeline")
+        .launch();
 
-      ClientArguments arguments = new ClientArguments(new String[]{
-        "--jar", "hdfs://ha-nn/pipelines/jars/ingest-gbif.jar",
-        "--class", "org.gbif.pipelines.ingest.pipelines.VerbatimToIdentifierPipeline",
-        "--arg", "--datasetId=13b70480-bd69-11dd-b15f-b8a03c50a862",
-        "--arg", "--attempt=432",
-        "--arg",
-        "--interpretationTypes=LOCATION,TEMPORAL,GRSCICOLL,MULTIMEDIA,BASIC,TAXONOMY,IDENTIFIER_ABSENT,AMPLIFICATION,IMAGE,CLUSTERING,OCCURRENCE,VERBATIM,MULTIMEDIA_TABLE,AUDUBON,LOCATION_FEATURE,MEASUREMENT_OR_FACT,METADATA",
-        "--arg", "--runner=SparkRunner",
-        "--arg", "--targetPath=hdfs://ha-nn/data/ingest",
-        "--arg", "--metaFileName=verbatim-to-identifier.yml",
-        "--arg", "--inputPath=hdfs://ha-nn/data/ingest/13b70480-bd69-11dd-b15f-b8a03c50a862/432/verbatim.avro",
-        "--arg", "--avroCompressionType=snappy",
-        "--arg", "--avroyncInterval=2097152",
-        "--arg", "--hdfsSiteConfig=/home/crap/config/hdfs-site.xml",
-        "--arg", "--coreSiteConfig=/home/crap/config/core-site.xml",
-        "--arg", "--properties=hdfs://ha-nn/pipelines/jars/pipelines.yaml",
-        "--arg", "--experiments=use_deprecated_read",
-        "--arg", "--tripletValid=true",
-        "--arg", "--occurrenceIdValid=true"
-      });
-
-      Client client = new Client(arguments, sparkConf);
-
-      YarnClientApplication application = yarnClient.createApplication();
-
-      ContainerLaunchContext containerLaunchContext =
-        ContainerLaunchContext.newInstance(null, null, null, null, null, null);
-
-      ApplicationSubmissionContext context =
-        client.createApplicationSubmissionContext(application, containerLaunchContext);
-
-      context.setApplicationName(jobId);
-      context.setQueue("root.download-admin");
-
-      ApplicationId applicationId = yarnClient.submitApplication(context);
-
-      return Optional.of(applicationId.toString());
-    } catch (YarnException | IOException ex) {
-      log.error("Error", ex);
-      return Optional.empty();
+      return getApplicationIdByName(jobId).stream().findAny().map(Objects::toString);
+    } catch (Exception ex) {
+      log.error("Oops", ex);
     }
+    return Optional.empty();
   }
 
   @Override
   public void cancelJob(@NotNull String jobId) {
     try {
-      for (ApplicationReport ar : yarnClient.getApplications(DOWNLOADS_TYPES, YARN_APPLICATION_STATES)) {
-        if (ar.getName().equals(jobId)) {
-          ApplicationId applicationId = ar.getApplicationId();
-          log.info("Killing jobId {}, aplicationId: {}", jobId, applicationId);
-          yarnClient.killApplication(applicationId);
-        }
+      for (ApplicationId applicationId : getApplicationIdByName(jobId)) {
+        log.info("Killing jobId {}, aplicationId: {}", jobId, applicationId);
+        yarnClient.killApplication(applicationId);
       }
     } catch (YarnException | IOException ex) {
       log.error("Exception during the killing the jobId {}", jobId, ex);
@@ -118,6 +84,21 @@ public class YarnJobManager implements JobManager {
     } catch (IOException ex) {
       log.error("Exception during the closing YARN client", ex);
     }
+  }
+
+  private List<ApplicationId> getApplicationIdByName(@NotNull String jobId) {
+    List<ApplicationId> ids = new ArrayList<>();
+    try {
+      for (ApplicationReport ar : yarnClient.getApplications(DOWNLOADS_TYPES, YARN_APPLICATION_STATES)) {
+        if (ar.getName().equals(jobId)) {
+          ApplicationId applicationId = ar.getApplicationId();
+          ids.add(applicationId);
+        }
+      }
+    } catch (YarnException | IOException ex) {
+      log.error("Exception during the killing the jobId {}", jobId, ex);
+    }
+    return ids;
   }
 
   // TODO: move to Spring bean
