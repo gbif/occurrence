8070d9597cdd4e7d4fd99d73cf7fd6c73157e395
Nikolay Volik <nikolay.volik@hotmail.com>
2024-01-11 14:15:11 +0100

    gbif/pipelines#993 Return previousIdentifications to termsPopulatedByInterpretation

--- occurrence-common/src/main/java/org/gbif/occurrence/common/TermUtils.java
+++ occurrence-common/src/main/java/org/gbif/occurrence/common/TermUtils.java
@@ -287,6 +287,7 @@ public class TermUtils {
       GbifTerm.isSequenced,
       GbifTerm.gbifRegion,
       GbifTerm.publishedByGbifRegion,
+      DwcTerm.previousIdentifications,
       DwcTerm.georeferencedBy,
       DwcTerm.higherGeography,
       DwcTerm.earliestEonOrLowestEonothem,
