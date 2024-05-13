b1fe05cf863d23e9394d08f24543665a651c0705
nvolik <nikolay.volik@hotmail.com>
2024-05-13 13:44:01 +0200

    gbif/pipelines#1034 Retrieve POM VERSION via mvn

--- build/occurrence-table-build-spark-docker-build.sh
+++ build/occurrence-table-build-spark-docker-build.sh
@@ -3,7 +3,13 @@
 set -e
 
 MODULE="occurrence-table-build-spark"
+
+POM_VERSION=$(mvn -q -Dexec.executable="echo" -Dexec.args='${project.version}' --non-recursive exec:exec)
+
+echo "Building Docker image module:version - ${MODULE}:${POM_VERSION}"
 docker build -f ./${MODULE}/docker/Dockerfile ./${MODULE} --build-arg JAR_FILE=${MODULE}-${POM_VERSION}-shaded.jar -t docker.gbif.org/${MODULE}:${POM_VERSION}
+
+echo "Pushing Docker image to the repository"
 docker push docker.gbif.org/${MODULE}:${POM_VERSION}
 if [[ $IS_M2RELEASEBUILD = true ]]; then
   echo "Updated latest tag pointing to the newly released docker.gbif.org/${MODULE}:${POM_VERSION}"
