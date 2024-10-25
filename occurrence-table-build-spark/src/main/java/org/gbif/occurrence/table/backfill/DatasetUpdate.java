eb351ff703f8f36b8cef4e4edd3e1cb068780ece
Dave Martin <djtfmartin@gmail.com>
2024-10-25 11:50:10 +0100

    backtick test

--- occurrence-table-build-spark/src/main/java/org/gbif/occurrence/table/backfill/DatasetUpdate.java
+++ occurrence-table-build-spark/src/main/java/org/gbif/occurrence/table/backfill/DatasetUpdate.java
@@ -142,12 +142,12 @@ public class DatasetUpdate {
     List<Column> columns = OccurrenceHDFSTableDefinition.definition().stream()
       .filter(field -> !field.getHiveField().equalsIgnoreCase("datasetkey")) //Partitioned columns must be at the end
       .map(field -> field.getInitializer().equals(field.getHiveField()) ?
-        col(field.getPipelinesAvroName())
-        : callUDF(
-              field.getInitializer().substring(0, field.getInitializer().indexOf("(")),
-              col(field.getPipelinesAvroName())
-          ).alias(field.getHiveField())
-       )
+          col("```" + field.getPipelinesAvroName() + "```")
+          : callUDF(
+          field.getInitializer().substring(0, field.getInitializer().indexOf("(")),
+          col("```" + field.getPipelinesAvroName() + "```")
+        ).alias(field.getHiveField())
+      )
       .collect(Collectors.toList());
 
     //Partitioned columns must be at the end
