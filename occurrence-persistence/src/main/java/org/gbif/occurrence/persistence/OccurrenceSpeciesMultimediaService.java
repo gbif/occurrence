f0daddc4a8a4da35968a6c8eb4b13003e7632a9a
fmendezh <federicomh@gmail.com>
2025-10-10 08:17:07 +0200

    using specific gets to avoid using scans

--- occurrence-persistence/src/main/java/org/gbif/occurrence/persistence/OccurrenceSpeciesMultimediaService.java
+++ occurrence-persistence/src/main/java/org/gbif/occurrence/persistence/OccurrenceSpeciesMultimediaService.java
@@ -1,11 +1,9 @@
 package org.gbif.occurrence.persistence;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.base.Preconditions;
 import lombok.SneakyThrows;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
-import org.apache.hadoop.hbase.filter.PrefixFilter;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.gbif.api.model.common.paging.PagingResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,69 +30,81 @@ public class OccurrenceSpeciesMultimediaService {
   private final TableName tableName;
   private final int splits;
   private final String paddingFormat;
-  private final int maxOffset;
+  private final int chunkSize;
   private final int maxLimit;
 
   @Autowired
   public OccurrenceSpeciesMultimediaService(Connection connection,
-                                            @Value("${occurrence.occurrenceSpeciesMediaTable}") String tableName,
-                                            @Value("${occurrence.occurrenceSpeciesMediaTableSplits}") int splits,
-                                            @Value("${occurrence.search.max.offset}") int maxOffset,
-                                            @Value("${occurrence.search.max.limit}") int maxLimit) {
+                                            @Value("${occurrence.db.speciesMultimedia.table}") String tableName,
+                                            @Value("${occurrence.db.speciesMultimedia.splits}") int splits,
+                                            @Value("${occurrence.db.speciesMultimedia.chunkSize}") int chunkSize,
+                                            @Value("${occurrence.db.speciesMultimedia.maxLimit}") int maxLimit) {
     this.connection = connection;
     this.tableName = TableName.valueOf(tableName);
     this.splits = splits;
     paddingFormat = "%0" + Integer.toString(splits - 1).length() + 'd';
     this.maxLimit = maxLimit;
-    this.maxOffset = maxOffset;
+    this.chunkSize = chunkSize;
   }
 
   @SneakyThrows
   public PagingResponse<SpeciesMediaType> queryIdentifiers(String speciesKey, String mediaType, int limitRequest, int offset) {
     // Validate and adjust limit and offset
     int limit = Math.min(limitRequest, maxLimit);
-    Preconditions.checkArgument(offset + limit <= maxOffset, "Max offset of " + maxOffset
-      + " exceeded: " + offset + " + " + limit);
 
+    int startChunk = offset / chunkSize;
+    int endChunk = (offset + limit - 1) / chunkSize;
+
+    var gets = getGets(speciesKey, mediaType, startChunk, endChunk);
     try (Table table = connection.getTable(tableName)) {
       Long totalCount = null;
       List<Map<String,Object>> results = new ArrayList<>();
-      //for (int salt = 0; salt < splits; salt++) {
-        byte[] prefix = (speciesKey + mediaType.toLowerCase(Locale.ROOT)).getBytes();
 
-        Scan scan = new Scan();
-        scan.setFilter(new PrefixFilter(prefix));
-        scan.addColumn(MEDIA_CF, MEDIA_INFOS);
-        scan.addColumn(MEDIA_CF, TOTAL_MULTIMEDIA_COUNTS);
-
-        try (ResultScanner scanner = table.getScanner(scan)) {
-          int skipped = 0;
-          for (Result result : scanner) {
-            byte[] value = result.getValue(MEDIA_CF, MEDIA_INFOS);
-            if (totalCount == null) {
-              byte[] byteTotalCount = result.getValue(MEDIA_CF, TOTAL_MULTIMEDIA_COUNTS);
-              if (byteTotalCount != null) {
-                totalCount = (long) Bytes.toInt(byteTotalCount);
-              }
-            }
-            if (value != null && results.size() < limit) {
-              List<Map<String, Object>> mediaInfos = MAPPER.readValue(Bytes.toString(value), List.class);
-              for (Map<String, Object> mediaInfo : mediaInfos) {
-                if (skipped < offset) {
-                  skipped++;
-                  continue;
-                }
-                if (results.size() < limit) {
-                  results.add(mediaInfo);
-                } else {
-                  break;
-                }
-              }
+      Result[] hbaseResults = table.get(gets);
+      int remaining = limit;
+      int currentOffset = offset % chunkSize;
+      for (Result result : hbaseResults) {
+        byte[] value = result.getValue(MEDIA_CF, MEDIA_INFOS);
+        if (totalCount == null) {
+          byte[] byteTotalCount = result.getValue(MEDIA_CF, TOTAL_MULTIMEDIA_COUNTS);
+          if (byteTotalCount != null) {
+            totalCount = (long) Bytes.toInt(byteTotalCount);
           }
+        }
+        if (value != null) {
+          List<Map<String, Object>> mediaInfos = MAPPER.readValue(Bytes.toString(value), List.class);
+          for (int i = currentOffset; i < mediaInfos.size() && remaining > 0; i++, remaining--) {
+            results.add(mediaInfos.get(i));
+          }
+          currentOffset = 0; // Only apply offset to the first chunk
         }
       }
       return new PagingResponse<>(offset, limit, totalCount,
                                  List.of(new SpeciesMediaType(speciesKey, mediaType, results)));
     }
+  }
+
+  private List<Get> getGets(String speciesKey, String mediaType, int startChunk, int endChunk) {
+    List<Get> gets = new ArrayList<>();
+    String mediaTypeValue = mediaType !=null? mediaType.toLowerCase(Locale.ROOT) : "";
+    for (int chunkIndex = startChunk; chunkIndex <= endChunk; chunkIndex++) {
+      String logicalKey = speciesKey + mediaTypeValue +  chunkIndex;
+      byte[] rowKey = computeKey(logicalKey);
+      gets.add(new Get(rowKey));
+    }
+    return gets;
+  }
+
+
+  /**
+   * Computes a salted key based on a expected number of buckets. The produced key is padded with
+   * zeros to the left + logicalKey.hasCode*numOfBuckets + logicalKey.
+   *
+   * @param logicalKey logical identifier
+   * @return a zeros left-padded string {0*}+bucketNumber+logicalKey
+   */
+  public byte[] computeKey(String logicalKey) {
+    return (String.format(paddingFormat, Math.abs(logicalKey.hashCode() % splits)) + logicalKey)
+      .getBytes(StandardCharsets.UTF_8);
   }
 }
