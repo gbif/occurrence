2909a0e6853bea3a62e8f046df513ea5b2538916
Marcos Lopez Gonzalez <marcoslg@gmail.com>
2026-01-10 12:01:34 +0100

    clarify projectID inclusion in DownloadTerms for distinct meanings in events and occurrences

--- occurrence-hdfs-table/src/main/java/org/gbif/occurrence/download/hive/DownloadTerms.java
+++ occurrence-hdfs-table/src/main/java/org/gbif/occurrence/download/hive/DownloadTerms.java
@@ -46,7 +46,10 @@ public class DownloadTerms {
           GbifInternalTerm.fragment, // omitted entirely
           GbifTerm.numberOfOccurrences,
           GbifInternalTerm.humboldtEventDurationValueInMinutes,
-          DwcTerm.projectID); // projectId is both in Dwc(events) and GbifTerm
+          DwcTerm.projectID // projectId is both in Dwc(events) and GbifTerm but it is included only
+          // one time since the column name is lowercased but it has different meaning in events and
+          // in occurrences
+          );
 
   /** This set is used for the HDFS table definition */
   public static final Set<Term> EXCLUSIONS_HDFS =
