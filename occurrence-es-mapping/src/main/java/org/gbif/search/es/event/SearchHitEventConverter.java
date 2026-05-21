a5984ed039260638cd540fe80644073685ada6e0
Marcos Lopez Gonzalez <marcoslg@gmail.com>
2026-05-21 10:17:08 +0200

    feat(DNA): #493 added locationID to the events search response

--- occurrence-es-mapping/src/main/java/org/gbif/search/es/event/SearchHitEventConverter.java
+++ occurrence-es-mapping/src/main/java/org/gbif/search/es/event/SearchHitEventConverter.java
@@ -273,7 +273,7 @@ public class SearchHitEventConverter extends SearchHitConverter<Event> {
     getListValueAsString(hit, PROJECT_TITLE).ifPresent(event::setProjectTitle);
     getListValueAsString(hit, FUNDING_ATTRIBUTION).ifPresent(event::setFundingAttribution);
     getListValueAsString(hit, FUNDING_ATTRIBUTION_ID).ifPresent(event::setFundingAttributionID);
-    getListValueAsString(hit, LOCATION_ID).ifPresent(event::setLocationID);
+    getStringValue(hit, LOCATION_ID).ifPresent(event::setLocationID);
     setEventLineageData(hit, event);
   }
 
