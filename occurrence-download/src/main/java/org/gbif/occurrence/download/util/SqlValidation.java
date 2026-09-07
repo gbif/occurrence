bd2e35dfab4b1d87a6072ba58e94c6729d768fc8
Dave Martin <djtfmartin@gmail.com>
2026-09-07 15:45:00 +0100

    feature(occurrence): work for #543

--- occurrence-download/src/main/java/org/gbif/occurrence/download/util/SqlValidation.java
+++ occurrence-download/src/main/java/org/gbif/occurrence/download/util/SqlValidation.java
@@ -341,7 +341,7 @@ public class SqlValidation {
           "species",
           "iucnredlistcategory",
           "taxonkeys",
-          "issues",
+          "taxonomicissue",
           "taxonomicstatus"
         )
       );
