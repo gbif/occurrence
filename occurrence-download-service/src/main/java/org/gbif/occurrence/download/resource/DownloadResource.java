74e950223674e14935decb4c61577764b484d031
Dave Martin <djtfmartin@gmail.com>
2026-09-11 12:35:54 +0100

    removed quotes from default denormalisedTaxonomy

--- occurrence-download-service/src/main/java/org/gbif/occurrence/download/resource/DownloadResource.java
+++ occurrence-download-service/src/main/java/org/gbif/occurrence/download/resource/DownloadResource.java
@@ -138,7 +138,7 @@ public class DownloadResource {
       DownloadType downloadType,
       @Value("${occurrence.download.disabled:false}") Boolean downloadsDisabled,
       @Value("${defaultChecklistKey}") String defaultChecklistKey,
-      @Value("${denormalisedTaxonomy: '7ddf754f-d193-4cc9-b351-99906754a03b'}") String denormalisedTaxonomy,
+      @Value("${denormalisedTaxonomy: 7ddf754f-d193-4cc9-b351-99906754a03b}") String denormalisedTaxonomy,
       @Value("${checklist.nested.struct.config:{}}") String checklistNestedStructMapJson) {
     this.archiveServerUrl = archiveServerUrl;
     this.requestService = service;
