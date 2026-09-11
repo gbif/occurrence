0743e8add6d9e264567ef5ba518a1a1518f9a2f2
Marcos Lopez Gonzalez <marcoslg@gmail.com>
2026-09-11 12:55:23 +0200

    #530 fixed lastModified format in download retrieval

--- occurrence-download-service/src/main/java/org/gbif/occurrence/download/resource/DownloadResource.java
+++ occurrence-download-service/src/main/java/org/gbif/occurrence/download/resource/DownloadResource.java
@@ -271,9 +271,7 @@ public class DownloadResource {
 
     String location = archiveServerUrl + downloadKey + extension;
     return ResponseEntity.status(HttpStatus.FOUND)
-        .header(
-            HttpHeaders.LAST_MODIFIED,
-            new SimpleDateFormat().format(new Date(downloadFile.lastModified())))
+        .lastModified(downloadFile.lastModified())
         .location(URI.create(location))
         .body(location);
   }
